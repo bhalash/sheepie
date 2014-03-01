7feb74765ad069539c1b55819303a8b4f000b072
bhalash <mark@bhalash.com>
2014-03-01 20:45:07 +0000

    Cleaned up jQuery.

--- design/functions.js
+++ design/functions.js
@@ -1,9 +1,7 @@
-a = $('#site-col-0');
-b = $('#site-col-1');
-c = $('#content');
-
 function leftContainerSize() {
     // Sets sidebar width @ 250px.
+    var a = $('#site-col-0');
+    var b = $('#site-col-1');
     a.css('width', 250);
     a.css('height', b.height()); 
 } 
@@ -11,13 +9,16 @@ function leftContainerSize() {
 function rightContainerSize() {
     // Right column set to window width - 300px.
     // There's a set min-width of 1000px.
+    var a = $('#site-col-0');
+    var b = $('#site-col-1');
     b.css('width', $(window).width() - a.width());
 }
 
 function commentTextarea() {
     // Sets the sizes for search form elements.
+    var mainCol = $('#content');
     var tx = $('#comment-entry textarea');
-    tx.css('width', $(c).width() - 8);
+    tx.css('width', $(mainCol).width() - 8);
     tx.attr('rows', 10);
 }
 
@@ -27,49 +28,75 @@ function socialWidgetHeight() {
     wid.css('height', wid.width());
 }
 
-function clearSearch(obj) {
-    // Toggles text in the search side bar. 
-    var string = 'Search RMWB';
+function clearInput(obj) {
+    // Clears the default value from inputs.
+    var inputStr = [
+        'Search RMWB',
+        'Name*',
+        'Email*',
+        'Website'
+    ]
 
-    if (obj.value == string) {
-        obj.value = '';
-    }
-    else if (obj.value == '') {
-        obj.value = string;
+    switch (obj.value) {
+        case inputStr[0]: 
+            clearValue(obj);
+            break;
+        case inputStr[1]:
+            clearValue(obj);
+            break;
+        case inputStr[2]: 
+            clearValue(obj);
+            break;
+        case inputStr[3]: 
+            clearValue(obj);
+            break;
+        default:
+            break;
     }
 }
 
-function clearComName(obj) {
-    var string = 'Name*';
+function clearValue(obj) {
+    obj.value = '';
+}
 
-    if (obj.value == string) {
-        obj.value = '';
-    }
-    else if (obj.value == '') {
-        obj.value = string;
+function restoreInput(obj) {
+    // Restore the default value to inputs if they are empty.
+    var inputName = [
+        'search-input',
+        'name-input',
+        'email-input',
+        'website-input'
+    ];
+
+    var inputStr = [
+        'Search RMWB',
+        'Name*',
+        'Email*',
+        'Website'
+    ]
+
+    if (obj.value == '') {
+        switch (obj.name) {
+            case inputName[0]: 
+                restoreValue(obj,inputStr[0]);
+                break;
+            case inputName[1]: 
+                restoreValue(obj,inputStr[1]);
+                break;
+            case inputName[2]: 
+                restoreValue(obj,inputStr[2]);
+                break;
+            case inputName[3]: 
+                restoreValue(obj,inputStr[3]);
+                break;
+            default:
+                break;
+        }
     }
 }
 
-function clearComEmail(obj) {
-    var string = 'Email*';
-
-    if (obj.value == string) {
-        obj.value = '';
-    }
-    else if (obj.value == '') {
-        obj.value = string;
-    }
-}
-
-function clearComWeb(obj) {
-    var string = 'Website';
-
-    if (obj.value == string) {
-        obj.value = '';
-    }
-    else if (obj.value == '') {
-        obj.value = string;
-    }
+function restoreValue(obj, str) {
+    obj.value = str;
 }
 
 $(document).ready(
