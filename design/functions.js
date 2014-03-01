bf81958625ba5b7424192be622227735a8dd7912
bhalash <mark@bhalash.com>
2014-03-01 04:24:54 +0000

    Updated search bar.

--- design/functions.js
+++ design/functions.js
@@ -1,45 +1,69 @@
-a = '#site-col-0';
-b = '#site-col-1';
-c = '#content';
+a = $('#site-col-0');
+b = $('#site-col-1');
+c = $('#content');
 
-function ColZeroSize() {
+function leftContainerSize() {
     // Sets sidebar width @ 250px.
-    $(a).css('width', 250);
-    $(a).css('height', $(b).height()); 
+    a.css('width', 250);
+    a.css('height', b.height()); 
 } 
 
-function ColOneSize() {
+function rightContainerSize() {
     // Right column set to window width - 300px.
     // There's a set min-width of 1000px.
-    $(b).css('width', $(window).width() - $(a).width());
+    b.css('width', $(window).width() - a.width());
 }
 
-function CommentFormSizes() {
+function commentTextarea() {
     // Sets the sizes for search form elements.
-    $('#comment-entry textarea').css('width', $(c).width() - 8);
-    $('#comment-entry textarea').attr('rows', 10);
+    var tx = $('#comment-entry textarea');
+    tx.css('width', $(c).width() - 8);
+    tx.attr('rows', 10);
 }
 
-function SidebarSearchWidth() {
-    // Sidebar search widget width. 
-    // Added extra padding, because Internet Explorer.
-	$('form [name=search').css('width', $('#sidebar').width() - 40);
+function socialWidgetHeight() {
+    var wid = $('#sidebar-social a *');
+    wid.css('width', wid.height());
+}
+
+function clearSearch(obj) {
+    var string = 'Search R.M.W.B.';
+    var button = $('form [name=search-submit]');
+
+    if (obj.value == string) {
+        obj.value = '';
+    }
+    else if (obj.value == '') {
+        obj.value = string;
+    }
+
+    if (button.hasClass('hidden')) {
+        button.removeClass('hidden');
+        button.addClass('shown');
+        button.fadeIn(1000);
+    }
+
+    else if (button.hasClass('shown')) {
+        button.removeClass('shown');
+        button.addClass('hidden');
+        button.fadeOut(1000);
+    }
 }
 
 $(document).ready(
     function() {
-	    ColZeroSize();
-        ColOneSize();
-        CommentFormSizes();
-	    SidebarSearchWidth();
+        commentTextarea();
+        leftContainerSize();
+        rightContainerSize();
+        socialWidgetHeight();
     }
 );
 
 $(window).on('resize',
     function() {
-        ColZeroSize();
-        ColOneSize();
-        CommentFormSizes();
-	    SidebarSearchWidth();
+        commentTextarea();
+        leftContainerSize();
+        rightContainerSize();
+        socialWidgetHeight();
     }
 );    
