d5623a7a14c067d00051fd43ad9cc38b2e56be72
bhalash <mark@bhalash.com>
2014-03-08 06:50:20 +0000

    Finished refactoring.

--- design/functions.js
+++ design/functions.js
@@ -1,21 +1,3 @@
-widthBreak = 830;
-
-function containerSizes() {
-    // var a = $('.content-left-exterior');
-    // var b = $('.content-right-exterior');
-
-    // if ($(window).width < widthBreak) {
-    //     a.css('width', '100%');
-    //     b.css('width', '100%');
-    // } else {
-    //     a.css('width', '250px');
-    //     b.css('width', $(window).width() - a.width());
-    // }
-
-    // // a.css('min-height', $(window).height() + 'px');
-    // b.css('width', ($(window).width() - a.width()) + 'px');
-}
-
 function commentTextarea() {
     // Sets the sizes for search form elements.
     var mainCol = $('#content');
@@ -31,21 +13,9 @@ function socialWidgetHeight() {
     soc.css('height', soc.width() + 'px'); 
 }
 
-// function clearSearch(obj) {
-//     // Clears and restores the default value from the search input.
-//     var str = 'Search RMWB';
-
-//     if (obj.value == str) {
-//         obj.value = '';
-//     } else if (obj.value == '') {
-//         obj.value = str;
-//     }
-// }
-
 $(document).ready(
     function() {
         // commentTextarea();
-        containerSizes();
         socialWidgetHeight();
     }
 );
@@ -53,7 +23,6 @@ $(document).ready(
 $(window).on('resize',
     function() {
         // commentTextarea();
-        containerSizes();
         socialWidgetHeight();
     }
 );    
