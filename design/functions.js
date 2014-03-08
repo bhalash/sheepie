9b3819ce4c2ea0a2e8092725642ee4e7a4a0b98f
bhalash <mark@bhalash.com>
2014-03-08 04:46:28 +0000

    Refactor update. Site is now 50% responsive.

--- design/functions.js
+++ design/functions.js
@@ -1,20 +1,19 @@
 widthBreak = 830;
 
 function containerSizes() {
-    var a = $('.content-left-exterior');
-    var b = $('.content-right-exterior');
+    // var a = $('.content-left-exterior');
+    // var b = $('.content-right-exterior');
 
-    if ($(window).width < widthBreak) {
-        a.css('width', '100%');
-        b.css('width', '100%');
-    } else {
-        a.css('width', '250px');
-        b.css('width', $(window).width() - a.width());
-    }
+    // if ($(window).width < widthBreak) {
+    //     a.css('width', '100%');
+    //     b.css('width', '100%');
+    // } else {
+    //     a.css('width', '250px');
+    //     b.css('width', $(window).width() - a.width());
+    // }
 
-    // a.css('min-height', $(window).height() + 'px');
-    b.css('width', ($(window).width() - a.width()) + 'px');
-
+    // // a.css('min-height', $(window).height() + 'px');
+    // b.css('width', ($(window).width() - a.width()) + 'px');
 }
 
 function commentTextarea() {
@@ -32,16 +31,16 @@ function socialWidgetHeight() {
     soc.css('height', soc.width() + 'px'); 
 }
 
-function clearSearch(obj) {
-    // Clears and restores the default value from the search input.
-    var str = 'Search RMWB';
+// function clearSearch(obj) {
+//     // Clears and restores the default value from the search input.
+//     var str = 'Search RMWB';
 
-    if (obj.value == str) {
-        obj.value = '';
-    } else if (obj.value == '') {
-        obj.value = str;
-    }
-}
+//     if (obj.value == str) {
+//         obj.value = '';
+//     } else if (obj.value == '') {
+//         obj.value = str;
+//     }
+// }
 
 $(document).ready(
     function() {
