d225d542efc454c383b17d6a4114ac2e82a81de9
bhalash <mark@bhalash.com>
2014-03-08 07:22:45 +0000

    Began work on Wordpress PHP.

---
+++ wordpress/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<title><?php wp_title('&laquo;', true, 'right'); bloginfo('name'); ?></title>
+<link rel="stylesheet" type="text/css" href="reset.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
