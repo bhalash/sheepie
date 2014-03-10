20be2d9d582c7b8073c3c232d60a676403fde5bd
bhalash <mark@bhalash.com>
2014-03-10 23:26:35 +0000

    Fixed incorrect input ID.
    id=author" => id="author"

--- rmwb/comments.php
+++ rmwb/comments.php
@@ -1,6 +1,6 @@
 <div class="post-comments">
     <?php if (post_password_required()) : ?>
-        <p>This post is password protected. Enter the password to view comments.</p>
+        <h3 class="comment-reply-title">This post is password protected. Enter the password to view comments.</h3>
     <?php return; endif; ?>
     <?php if (have_comments()) : ?>
         <h3 class="comment-reply-title"><?php comments_number('No Comments:', '1 Comment:', '% Comments:');?></h3>
@@ -15,7 +15,7 @@
         <?php if (comments_open()) : ?>
             <?php else : ?>
                 <?php if(!is_page()) : ?>
-                    <p class="nocomments">Comments are closed.</p>
+                    <h3 class="comment-reply-title">Comments are closed.</h3>
                 <?php endif; ?>
         <?php endif; ?>
     <?php endif; ?>
@@ -23,7 +23,7 @@
 <div class="comment-entry">
     <?php 
     $comment_form_fields = array (
-        'author' => '<input aria-required="true" class="comment-form-author" id=author" name="author" placeholder="Name*" type="text">',
+        'author' => '<input aria-required="true" class="comment-form-author" id="author" name="author" placeholder="Name*" type="text">',
         'email' => '<input aria-required="true" class="comment-form-email" id="email" name="email" placeholder="Email*" type="email">',
         'url' => '<input aria-required="true" class="comment-form-url" id="url" name="url" placeholder="Website" type="url">'
         );
