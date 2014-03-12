89c4ea463259da054652247546ab4c5b0ead23a1
bhalash <mark@bhalash.com>
2014-03-12 05:16:26 +0000

    Bug fixes.

--- rmwb/functions.js
+++ rmwb/functions.js
@@ -8,14 +8,37 @@ function navigationPrevWidth() {
     navR.css('width', navL.outerWidth() + 'px');    
 }
 
+function rightMinHeight() {
+    // Page height - article bottom-margin.
+    var rCol = $('.content-right-interior');
+    rCol.css('min-height', ($(window).innerHeight() - 30) + 'px');
+}
+
+$(function() {
+    // Page navigation colours. Red down, blue up.
+    var nla = $('.nav-left a, .nav-right a' );
+    var wid = nla.outerWidth();
+
+    nla.hover( 
+        function() {
+            $(this).animate({'width' : wid * 1.2}, 100);
+            // $(this).stop().animate({backgroundColor: '#fff'}, 300);
+        }, function() {
+            $(this).animate({'width' : wid}, 100);
+            // $(this).stop().animate({backgroundColor: '#000'}, 300);
+    })
+});
+
 $(document).ready(
     function() {
         navigationPrevWidth();
+        rightMinHeight();
     }
 );
 
 $(window).on('resize',
     function() {
         navigationPrevWidth();
+        rightMinHeight();
     }
 );
