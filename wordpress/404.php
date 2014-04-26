66954885a8914a5ac761699f3a37d34375ff41fa
Mark <mark@bhalash.com>
2014-04-26 22:49:44 +0100

    Tweaked 404 error

--- wordpress/404.php
+++ wordpress/404.php
@@ -1,8 +1,8 @@
 <?php get_header();  ?>
 <div class="content-right">
     <div class="content-right-interior">
-        <article>
-            <h3>Four, Oh Four!</h3>
+        <article <?php post_class(); ?>>
+            <h3 class="post-title">Four, Oh Four!</h3>
             <p>Page not found.</p>
         </article>
     <?php get_footer(); ?>
