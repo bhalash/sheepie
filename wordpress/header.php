6c536972a6f3e4f2295048cff0a1a1b880b21349
Mark <mark@bhalash.com>
2014-05-09 20:56:12 +0100

    Because

--- wordpress/header.php
+++ wordpress/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<!-- Bless the Maker and His water. Bless the coming and going of Him. May His passage cleanse the world. May He keep the world for His people. -->
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php wp_title('&laquo;', true, 'right'); bloginfo('name'); ?></title>
