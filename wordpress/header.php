77a9c34ed100e8104c36a5c3de1fad429fd45cb5
Mark <mark@bhalash.com>
2014-05-15 21:16:58 +0100

    Added website favicon

--- wordpress/header.php
+++ wordpress/header.php
@@ -6,6 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php wp_title('&laquo;', true, 'right'); bloginfo('name'); ?></title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/img/icon.png" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <?php wp_head(); ?>
 </head>
