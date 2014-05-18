c3faffcb6399d7d82f89c8413703f903eccdc467
Mark <mark@bhalash.com>
2014-05-18 18:33:27 +0100

    Altered mobile appearance
    The page didn't need to be able to be zoomed.

--- wordpress/header.php
+++ wordpress/header.php
@@ -3,7 +3,7 @@
 <head>
 <!-- Bless the Maker and His water. Bless the coming and going of Him. May His passage cleanse the world. May He keep the world for His people. -->
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 <title><?php wp_title('&laquo;', true, 'right'); bloginfo('name'); ?></title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/img/icon.png" />
