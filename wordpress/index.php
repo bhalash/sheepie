1b096c8f798787d0373bc0b1407791a16ca73c97
Mark <mark@bhalash.com>
2014-05-24 04:50:42 +0100

    Corrected datetime attribute on posts

--- wordpress/index.php
+++ wordpress/index.php
@@ -9,7 +9,7 @@
                 </h3>
                 <p class="post-meta-top">
                     Posted by <a title="Find more posts by <?php the_author(); ?>" href="<?php echo home_url(); ?>/?s&author=<?php the_author(); ?>"><?php the_author(); ?></a> 
-                    on <time><?php the_time(get_option('date_format')) ?>.</time> <?php comments_popup_link('No comments.', '1 Comment.', '% Comments.'); ?>
+                    on <time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?>.</time> <?php comments_popup_link('No comments.', '1 Comment.', '% Comments.'); ?>
                     <?php edit_post_link('Edit post.', ' ', ''); ?>
                 </p>
                 <?php the_content('Read the rest of this post &raquo;'); ?>
