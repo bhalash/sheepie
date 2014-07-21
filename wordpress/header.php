b61be57633bdbc5561e8884254b847f1cb5fda3c
Mark <mark@bhalash.com>
2014-07-21 18:26:39 +0100

    Renamed site sections
    Cured some divitis by shifting from div to section.

--- wordpress/header.php
+++ wordpress/header.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<!-- Bless the Maker and His water. Bless the coming and going of Him. May His passage cleanse the world. May He keep the world for His people. -->
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-<title><?php wp_title('&laquo;', true, 'right'); bloginfo('name'); ?></title>
-<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro|Source+Code+Pro" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/img/icon.png" />
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-<?php wp_head(); ?>
+    <!-- Bless the Maker and His water. Bless the coming and going of Him. May His passage cleanse the world. May He keep the world for His people. -->
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title><?php wp_title('&laquo;', true, 'right'); bloginfo('name'); ?></title>
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro|Source+Code+Pro" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/img/icon.png" />
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <div class="site-container">
-        <div class="content-left">
-            <div class="content-left-interior">
+    <section class="site-container">
+        <section class="content-left">
+            <div class="interior">
                 <header>
                     <ul>
                         <?php split_title(get_bloginfo('name')); ?>
                     </ul>
+<!--                     <ul class="social">
+                        <li><a class="social-widget" id="twitter" title="Follow @bhalash on Twitter" href="http://www.twitter.com/bhalash"></a></li>
+                        <li><a class="social-widget" id="facebook" title="Contact Mark on Facebook" href="http://www.facebook.com/bhalash"></a></li>
+                        <li><a class="social-widget" id="instagram" title="Follow @bhalash on Instagram" href="http://www.instagram.com/bhalash"></a></li>
+                        <li><a class="social-widget" id="github" title="Friend Bhalash on GitHub" href="http://www.github.com/bhalash"></a></li>
+                    </ul>
+ -->                    <?php get_search_form(); ?>
                 </header>
-                <ul class="social">
-                    <li><a class="social-widget" id="twitter" title="Follow @bhalash on Twitter" href="http://www.twitter.com/bhalash"></a></li>
-                    <li><a class="social-widget" id="facebook" title="Contact Mark on Facebook" href="http://www.facebook.com/bhalash"></a></li>
-                    <li><a class="social-widget" id="instagram" title="Follow @bhalash on Instagram" href="http://www.instagram.com/bhalash"></a></li>
-                    <li><a class="social-widget" id="github" title="Friend Bhalash on GitHub" href="http://www.github.com/bhalash"></a></li>
-                </ul>
-                <?php get_search_form(); ?>
                 <?php get_sidebar(); ?>
             </div>
-        </div> 
+        </section> 
--- wordpress/index.php
+++ wordpress/index.php
@@ -1,32 +1,35 @@
-<?php get_header();  ?>
-<div class="content-right">
-    <div class="content-right-interior">
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                <h3 class="post-title">
-                    <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                </h3>
-                <p class="post-meta-top">
-                    Posted by <a title="Find more posts by <?php the_author(); ?>" href="<?php echo home_url(); ?>/?s&author=<?php the_author(); ?>"><?php the_author(); ?></a> 
-                    on <time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?>.</time> <?php comments_popup_link('No comments.', '1 Comment.', '% Comments.'); ?>
-                    <?php edit_post_link('Edit post.', ' ', ''); ?>
-                </p>
-                <?php the_content('Read the rest of this post &raquo;'); ?>
-            </article>
-            <hr>
-        <?php endwhile; else: ?>
+<?php get_header(); ?>
+<section class="content-right">
+    <div class="interior">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                    <h3 class="post-title">
+                        <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                    </h3>
+                    <p class="post-meta-top">
+                        By <a title="Find more posts by <?php the_author(); ?>" href="<?php echo home_url(); ?>/?s&author=<?php the_author(); ?>"><?php the_author(); ?></a> 
+                        on <time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?> in <?php the_category(', '); ?>.</time> <?php comments_popup_link('', '1 Comment.', '% Comments.'); ?>
+                        <?php edit_post_link('Edit post.', ' ', ''); ?>
+                    </p>
+                    <?php the_content('Read the rest of this post &raquo;'); ?>
+                </article>
+                <hr />
+            <?php endwhile; ?>
+        <?php else: ?>
             <article> 
                 <h3>No joy, sorry</h3>
                 <p>Sorry, no posts were found that match your criteria!</p>
             </article>
         <?php endif; ?>
-        <div class="site-nav">
-            <div class="nav-left">
+        <section class="pagination">
+            <div class="left">
                 <?php previous_posts_link('Previous Page'); ?>
             </div>
-            <div class="nav-right">
+            <div class="right">
                 <?php next_posts_link('Next Page'); ?>
             </div>
-        </div>
-        <?php get_footer(); ?>
+        </section>
+    </div>
+</section>
+<?php get_footer(); ?>
