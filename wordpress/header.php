053932b208883dffd5e68958e8c40b260eba3c4a
Mark <mark@bhalash.com>
2014-07-22 16:51:09 +0100

    Uncommented social menu

--- wordpress/header.php
+++ wordpress/header.php
@@ -19,13 +19,13 @@
                     <ul>
                         <?php split_title(get_bloginfo('name')); ?>
                     </ul>
-<!--                     <ul class="social">
+                    <ul class="social">
                         <li><a class="social-widget" id="twitter" title="Follow @bhalash on Twitter" href="http://www.twitter.com/bhalash"></a></li>
                         <li><a class="social-widget" id="facebook" title="Contact Mark on Facebook" href="http://www.facebook.com/bhalash"></a></li>
                         <li><a class="social-widget" id="instagram" title="Follow @bhalash on Instagram" href="http://www.instagram.com/bhalash"></a></li>
                         <li><a class="social-widget" id="github" title="Friend Bhalash on GitHub" href="http://www.github.com/bhalash"></a></li>
                     </ul>
- -->                    <?php get_search_form(); ?>
+                    <?php get_search_form(); ?>
                 </header>
                 <?php get_sidebar(); ?>
             </div>
