71e0e3e5f4999cea2eba4e9fc28bdead5d6d906e
Mark <mark@bhalash.com>
2014-07-23 19:19:12 +0100

    Cured some divitis
    * Removed excessive and specific tagging.
    * Removed <hr /> between posts.

--- wordpress/index.php
+++ wordpress/index.php
@@ -3,18 +3,17 @@
     <div class="interior">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
-                <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                    <h3 class="post-title">
+                <article <?php post_class(); ?> id="article-<?php the_ID(); ?>">
+                    <h3 class="article-title">
                         <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
                     </h3>
-                    <p class="post-meta-top">
-                        By <a title="Find more posts by <?php the_author(); ?>" href="<?php echo home_url(); ?>/?s&author=<?php the_author(); ?>"><?php the_author(); ?></a> 
+                    <p class="meta">
+                        by <a title="Find more posts by <?php the_author(); ?>" href="<?php echo home_url(); ?>/?s&author=<?php the_author(); ?>"><?php the_author(); ?></a> 
                         on <time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?> in <?php the_category(', '); ?>.</time> <?php comments_popup_link('', '1 Comment.', '% Comments.'); ?>
                         <?php edit_post_link('Edit post.', ' ', ''); ?>
                     </p>
                     <?php the_content('Read the rest of this post &raquo;'); ?>
                 </article>
-                <hr />
             <?php endwhile; ?>
         <?php else: ?>
             <article> 
--- wordpress/single.php
+++ wordpress/single.php
@@ -3,22 +3,20 @@
     <div class="interior">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
-                <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                    <h3 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-                    <p class="post-meta-top">
-                        By <a title="Find more posts by <?php the_author(); ?>" href="<?php echo home_url(); ?>/?s&author=<?php the_author(); ?>"><?php the_author(); ?></a> 
+                <article <?php post_class(); ?> id="article-<?php the_ID(); ?>">
+                    <h3 class="article-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+                    <p class="meta">
+                        by <a title="Find more posts by <?php the_author(); ?>" href="<?php echo home_url(); ?>/?s&author=<?php the_author(); ?>"><?php the_author(); ?></a> 
                         on <time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?> in <?php the_category(', '); ?>.</time> <?php comments_popup_link('', '1 Comment.', '% Comments.'); ?>
                         <?php edit_post_link('Edit post.', ' ', ''); ?>
                     </p>
                     <?php the_content('Read the rest of this post &raquo;'); ?>
-                    <p class="post-meta-bottom">
-                        <strong>Tagged:</strong> <?php the_tags('', ', ' ,  ''); ?><br />
-                    </p>
+                    <p class="meta">Tagged: <?php the_tags('', ', ' ,  ''); ?></p>
                 </article>
             <?php endwhile; ?> 
         <?php else: ?>
             <article> 
-                <h3 class="post-title">No joy, sorry</h3>
+                <h3 class="article-title">No joy, sorry</h3>
                 <p>Sorry, no posts were found that match your criteria!</p>
             </article>
         <?php endif; ?>
