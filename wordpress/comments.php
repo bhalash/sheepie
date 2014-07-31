f8f686404a861fcda1e657774ebaf4e1cbbbe81b
Mark <mark@bhalash.com>
2014-07-31 20:06:31 +0100

    Wrapped comments in loop to see if they're open

--- wordpress/comments.php
+++ wordpress/comments.php
@@ -1,41 +1,36 @@
-<div class="article-comments" id="comments">
-    <?php if (post_password_required()) : ?>
-        <h3 class="article-title">This post is password protected. Enter the password to view comments.</h3>
-    <?php return; endif; ?>
-    <?php if (have_comments()) : ?>
-        <h3 class="comment-title"><?php comments_number('% comments.', '1 comment.', '% comments.');?></h3>
-        <ul>
-            <?php wp_list_comments(
-                array( 
-                    'callback' => 'rmwb_comments',
-                    'avatar_size' => 0,
-                    'format' => 'html5',
-                    'style' => 'ul'
-                )
-            ); ?>
-        </ul>
-     <?php else : ?>
-        <?php if (comments_open()) : ?>
-            <?php else : ?>
-                <?php if(!is_page()) : ?>
-                    <h3>Comments are closed.</h3>
-                <?php endif; ?>
+<?php if (comments_open()) : ?>
+    <div class="article-comments" id="comments">
+        <?php if (post_password_required()) : ?>
+            <h3 class="article-title">This post is password protected. Enter the password to view comments.</h3>
+        <?php return; endif; ?>
+        <?php if (have_comments()) : ?>
+            <h3 class="comment-title"><?php comments_number('% comments.', '1 comment.', '% comments.');?></h3>
+            <ul>
+                <?php wp_list_comments(
+                    array( 
+                        'callback' => 'rmwb_comments',
+                        'avatar_size' => 0,
+                        'format' => 'html5',
+                        'style' => 'ul'
+                    )
+                ); ?>
+            </ul>
         <?php endif; ?>
-    <?php endif; ?>
-</div>
-<div class="comment-entry">
-    <?php 
-    $comment_form_fields = array (
-        'author' => '<input aria-required="true" class="comment-form-author" id="author" name="author" placeholder="Name*" type="text">',
-        'email' => '<input aria-required="true" class="comment-form-email" id="email" name="email" placeholder="Email*" type="email">',
-        'url' => '<input aria-required="true" class="comment-form-url" id="url" name="url" placeholder="Website" type="url">'
-        );
+    </div>
+    <div class="comment-entry">
+        <?php 
+        $comment_form_fields = array (
+            'author' => '<input aria-required="true" class="comment-form-author" id="author" name="author" placeholder="Name*" type="text">',
+            'email' => '<input aria-required="true" class="comment-form-email" id="email" name="email" placeholder="Email*" type="email">',
+            'url' => '<input aria-required="true" class="comment-form-url" id="url" name="url" placeholder="Website" type="url">'
+            );
 
-    comment_form( 
-        array (
-            'title_reply' => 'Say something',
-            'comment_field' => '<p><textarea aria-required="true" class="comment-form-comment" id="comment" name="comment" rows="10"></textarea></p>',
-            'fields' => $comment_form_fields
-        )
-    ); ?>
-</div>
+        comment_form( 
+            array (
+                'title_reply' => 'Say something',
+                'comment_field' => '<p><textarea aria-required="true" class="comment-form-comment" id="comment" name="comment" rows="10"></textarea></p>',
+                'fields' => $comment_form_fields
+            )
+        ); ?>
+    </div>
+<?php endif; ?>
