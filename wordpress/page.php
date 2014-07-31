436fc1b84a5c16e2e5e3908d625bc5f1d6b08082
Mark <mark@bhalash.com>
2014-07-31 20:07:41 +0100

    Removed article-title tag

--- wordpress/page.php
+++ wordpress/page.php
@@ -4,7 +4,7 @@
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
                 <article <?php post_class(); ?> id="article-<?php the_ID(); ?>">
-                    <h2 class="article-title">
+                    <h2>
                         <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
                     </h2>
                     <?php the_content('Read the rest of this post &raquo;'); ?>
@@ -12,7 +12,7 @@
             <?php endwhile; ?> 
         <?php else: ?>
             <article> 
-                <h3 class="article-title">No joy, sorry</h3>
+                <h3>No joy, sorry</h3>
                 <p>Sorry, no posts were found that match your criteria!</p>
             </article>
         <?php endif; ?>
