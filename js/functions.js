69618f543a390a0460117f99b543b54f8ca45094
Mark <mark@bhalash.com>
2014-12-26 02:43:09 +0000

    Updated script to properly run under WordPress

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,26 @@
-function contentHeight() {
-    // Page height - article bottom-margin.
-    $('.right-wrap').css('min-height', $(window).height() + 'px');
-}
-
-function setSearchBoxText() {
-    // Inserts the search query string into the search box. 
-    var searchBox = '.right-wrap .search-input';
-    var queryBox = '.search-query-hidden';
-
-    if ($(searchBox).length > 0 && $(queryBox).length > 0) {
-        var query = $(queryBox).text();
-        $(queryBox).remove();
-        $(searchBox).val(query);
-        $(searchBox).focus();
+jQuery(function($) {
+    function contentHeight() {
+        // Page height - article bottom-margin.
+        $('.right-wrap').css('min-height', $(window).height() + 'px');
     }
-}
 
-$(function() {
+    function setSearchBoxText() {
+        // Inserts the search query string into the search box. 
+        var searchBox = '.right-wrap .search-input';
+        var queryBox = '.search-query-hidden';
+
+        if ($(searchBox).length > 0 && $(queryBox).length > 0) {
+            var query = $(queryBox).text();
+            $(queryBox).remove();
+            $(searchBox).val(query);
+            $(searchBox).focus();
+        }
+    }
+
     // For google-code-prettify
     $('pre').each(function() {
         $(this).addClass('prettyprint');
     });    
-
-    $('.current-menu-item').prev().css('margin-bottom', '10px');
-    $('.current-menu-item').next().css('margin-top', '10px');
 
     contentHeight();
     
@@ -37,8 +34,8 @@ $(function() {
     });
 
     setSearchBoxText();
-});
 
-$(window).resize(function() {
-    contentHeight();
+    $(window).resize(function() {
+        contentHeight();
+    });
 });
