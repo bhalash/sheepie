4f2738bc1cd9154587ce254676cea1b3c9fb7d9f
Mark <mark@bhalash.com>
2015-02-08 00:51:34 +0000

    Rewriting searchform and search results
    Definitely WIP

---
+++ page-search.php
@@ -0,0 +1,4 @@
+<?php /* Template Name: Search Form */ ?>
+<?php get_header(); ?>
+<?php get_search_form(); ?>
+<?php get_footer(); ?>
--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form action="<?php echo get_site_url(); ?>" class="site-search" method="get">
+<form action="<?php echo get_site_url(); ?>" id="search" method="get">
     <p>
-        <input id="s" name="s" placeholder="Search" tabindex="1" type="text" />
-        <input type="submit" value="" />
+        <input name="s" placeholder="Search the site" tabindex="1" type="text" value="<?php echo get_search_query(); ?>" />
+        <button type="submit"></button>
     </p>
 </form>
