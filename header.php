732828485fd78a6738d2bda14c53ac599c2309de
Mark <mark@bhalash.com>
2015-02-08 00:44:26 +0000

    Updated header
    * Added content container opening tags.
    * Added placeholders for social and navigation menus.
    * Added switches to either close the page or present information for
    users who aren’t logged in.
    * Added draft placeholders for sitewide titles.

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+<?php 
+// Better than any maintenance mode plugin.
+if (!is_user_logged_in()) {
+    // header('HTTP/1.1 503 Service Unavailable');
+    // header('Retry-After: 3600');
+    // exit();
+}
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -7,29 +15,51 @@
     <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
     <title>
         <?php 
-            if ($paged > 1) { 
-                echo 'Page ' . $paged . ' | '; 
+            if (is_home()) {
                 echo bloginfo('name');
-            } else {
-                wp_title('|', true, 'right'); 
-                echo bloginfo('name');
-            } 
+            }
+
+            // if ($paged > 1) { 
+            //     echo 'Page ' . $paged . ' | '; 
+            //     echo bloginfo('name');
+            // } else {
+            //     wp_title('|', true, 'right'); 
+            // } 
         ?>
     </title>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-    <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/img/icon.png" />
+    <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>  onload="prettyPrint()">
-    <div class="site-container">
-        <div class="left-wrap">
-            <div class="content">
-                <div id="header">
-                    <ul class="title">
-                        <?php split_title(get_bloginfo('name')); ?>
-                    </ul>
-                    <?php get_search_form(); ?>
+    <div id="main">
+        <div id="header">
+            <div class="mask">
+                <div class="interior">
+                    <h3><a title="Go home" href="<?php echo site_url(); ?>"><?php echo get_bloginfo('name'); ?></a></h3>
+                    <div class="description">
+                        <p><?php echo get_bloginfo('description'); ?></p>
+                        <nav class="menu">
+                            <ul>
+                                <li><a title="About me" href="/about/">About</a></li>
+                                <li><a title="Blog archives" href="/archives/">Archives</a></li>
+                                <li><a title="Search the site" href="javascript:void(0)">Search</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <nav class="social">
+                        <ul>
+                            <li class="email"><a title="" href="mailto:mark@bhalash.com"></a></li>
+                            <li class="twitter"><a title="" href="//www.twitter.com/bhalash"></a></li>
+                            <li class="github"><a title="" href="//www.github.com/bhalash"></a></li>
+                        </ul>
+                    </nav>
                 </div>
-                <?php get_sidebar(); ?>
             </div>
         </div>
+        <div id="content">
+            <div class="interior">
+                <?php if (!is_user_logged_in()) : ?>
+                    <!-- TEMPORARY -->
+                    <div class="dev-warning"></div>
+                <?php endif; ?>
