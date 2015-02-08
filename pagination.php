d7e055f9b5c44923c5907f5e6b81a714ce9317b8
Mark <mark@bhalash.com>
2015-02-08 00:47:31 +0000

    Moved pagination to its own file

---
+++ pagination.php
@@ -0,0 +1,8 @@
+<div id="pagination">
+    <div class="page-left">
+        <?php previous_posts_link('&#8592; Previous Page'); ?>
+    </div>
+    <div class="page-right">
+        <?php next_posts_link('Next Page &#8594;'); ?>
+    </div>
+</div>
