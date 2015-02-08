a0846d116f1215271e940c3b2f5a65523ec58dc3
Mark <mark@bhalash.com>
2015-02-08 00:50:42 +0000

    Split off article templates
    As with single.php and page.php, this now calls the article templates
    instead of having the content inline.

--- index.php
+++ index.php
@@ -1,45 +1,20 @@
-<?php get_header(); ?>
-<div class="right-wrap">
-    <div class="content">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <article <?php post_class(); ?> id="article-<?php the_ID(); ?>">
-                    <h2>
-                        <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                    </h2>
-                    <h6>
-                        posted in <?php the_category(', '); ?>
-                        <?php if ( comments_open() ) : ?>
-                            with <a href="<?php comments_link(); ?>"><?php comments_number('0 comments', '1 comment', '% comments'); ?></a>
-                        <?php endif; ?>
-                    </h6>
-                    <?php the_content('Read the rest of this post &raquo;'); ?>
-                    <p class="meta">
-                        <?php $year = get_the_time('Y'); ?>
-                        <?php $month = get_the_time('m'); ?>
-                        <small>
-                            by <a title="Find more posts by <?php the_author(); ?>" href="<?php echo home_url(); ?>/?s&author=<?php the_author(); ?>"><?php the_author(); ?></a>
-                            - <time datetime="<?php the_time('Y-m-d H:i'); ?>"><a href="<?php echo get_month_link($year, $month); ?>"><?php the_time(get_option('date_format')) ?></a></time>
-                            <br />
-                            <?php edit_post_link('Edit post.', ' ', ''); ?>
-                        </small>
-                    </p>
-                </article>
-            <?php endwhile; ?>
-        <?php else: ?>
-            <article> 
-                <h3>No joy, sorry</h3>
-                <p>Sorry, no posts were found that match your criteria!</p>
-            </article>
-        <?php endif; ?>
-        <div class="pagination">
-            <div class="left">
-                <?php previous_posts_link('&#8592; Previous Page'); ?>
-            </div>
-            <div class="right">
-                <?php next_posts_link('Next Page &#8594;'); ?>
-            </div>
-        </div>
-    </div>
-</div>
-<?php get_footer(); ?>
+<?php get_header();
+
+    $count = 0;
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+
+            if ($count > 0) {
+                printf('<hr />');
+            }
+                
+            get_template_part('article', 'full');
+            $count++;
+        }
+    } else {
+        get_template_part('article','missing');
+    }
+
+include('pagination.php');
+get_footer(); ?>
