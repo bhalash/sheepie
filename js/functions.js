810726e7c7dd135ca5ee14e0398db3ff63170be7
Mark <mark@bhalash.com>
2015-02-08 00:50:02 +0000

    Removed unnecessary JavaScript code
    \o/ Sass \o/

--- js/functions.js
+++ js/functions.js
@@ -1,41 +1,16 @@
 jQuery(function($) {
-    function contentHeight() {
-        // Page height - article bottom-margin.
-        $('.right-wrap').css('min-height', $(window).height() + 'px');
-    }
-
-    function setSearchBoxText() {
-        // Inserts the search query string into the search box. 
-        var searchBox = '.right-wrap .search-input';
-        var queryBox = '.search-query-hidden';
-
-        if ($(searchBox).length > 0 && $(queryBox).length > 0) {
-            var query = $(queryBox).text();
-            $(queryBox).remove();
-            $(searchBox).val(query);
-            $(searchBox).focus();
-        }
-    }
-
     // For google-code-prettify
     $('pre').each(function() {
         $(this).addClass('prettyprint');
     });    
 
-    contentHeight();
-    
-    // Remove underline from hyperlink images. 
-    $('p > a').each(function() {
-        if ($(this).children('img').length > 0) {
-            $(this).hover(function() {
-                $(this).css('border-style', 'none');
-            });
-        }
-    });
+    $('#search').submit(function() {
+        $(':submit').attr('disabled', true);
+    })
 
-    setSearchBoxText();
+    var devUrl = 'http://www.bhalash.com/archives/13544803444';
+    var devWarning = '<p><strong>Feb 2015:</strong> Some parts of the site may be broken, as it is under active redevelopment. See <a href="' + devUrl + '">this</a> post for more information.';
 
-    $(window).resize(function() {
-        contentHeight();
-    });
+    // Warning for users while I work on the site.
+    $('.dev-warning').html(devWarning);
 });
