b4836ac89ddacd9a21295291ed19124a9234fea1
Mark <mark@bhalash.com>
2015-02-09 02:21:38 +0000

    Added search and commentform functions
    * Search functions open and focus it on page or search load.
    * Commentform toggle unhides all elements of the form.

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,74 @@
 jQuery(function($) {
-    // For google-code-prettify
-    $('pre').each(function() {
-        $(this).addClass('prettyprint');
-    });    
+    /*
+     * Search Form Focusing
+     * --------------------
+     */
 
-    $('#search').submit(function() {
-        $(':submit').attr('disabled', true);
-    })
+    var search = {
+        form: '.search',
+        input: '.search-text',
+        results: '.page-search',
+        url: window.location.host + '/search/',
+        nav: {
+            toggle: '#nav-search-toggle',
+            body: '.nav-search',
+            open: false
+        }
+    };
+
+    if (search.results.length > 0) {
+        $(search.results).find(search.input).focus();
+    }
+
+    $(search.nav.toggle).click(function() {
+        $(search.nav.body).slideDown(200, function() {
+            $(search.nav.body).find(search.input).focus();
+        });
+
+        if (!search.nav.open) {
+            search.nav.open = true;
+            return false;
+        }
+    });
+
+    $(search.form).submit(function(click) {
+        if ($(search.input).val() === '') {
+            click.preventDefault();
+            window.location.href = search.url;
+        }
+    });
+
+    /*
+     * User Warning
+     * ------------
+     * Warning for users while I work on the site.
+     */
 
     var devUrl = 'http://www.bhalash.com/archives/13544803444';
     var devWarning = '<p><strong>Feb 2015:</strong> Some parts of the site may be broken, as it is under active redevelopment. See <a href="' + devUrl + '">this</a> post for more information.';
-
-    // Warning for users while I work on the site.
     $('.dev-warning').html(devWarning);
+
+    /*
+     * Commentform Toggle
+     * ------------------
+     * I've decided to hide the commentform unless the user chooses to use it.
+     */
+
+    $('#textarea textarea').focus(function() {
+        $(this).css('height', 150);
+
+        $('#commentform > *').slideDown(200, function() {
+            $(window).scrollTop($('#commentform').offset().top);
+        });
+    });
+
+    /*
+     * Google Code Prettify
+     * --------------------
+     * Add a suitable class to each pre block for Google Code Prettifier.
+     */
+
+    $('pre').each(function() {
+        $(this).addClass('prettyprint');
+    });    
 });
