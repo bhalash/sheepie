e4f153e8e6fce7829d050701b7a5f73f19f91563
Mark <mark@bhalash.com>
2015-02-09 02:19:35 +0000

    Changed searchform the id to class
    There are cases where two will be present on a page, so replying on the
    ID is a Bad Idea.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form action="<?php echo get_site_url(); ?>" id="search" method="get">
+<form class="search" action="<?php echo get_site_url(); ?>" method="get">
     <p>
-        <input name="s" placeholder="Search the site" tabindex="1" type="text" value="<?php echo get_search_query(); ?>" />
+        <input type="text" class="search-text" name="s" tabindex="1" placeholder="Search the site" value="<?php echo get_search_query(); ?>" />
         <button type="submit"></button>
     </p>
 </form>
