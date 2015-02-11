100b2e1a176674480f8f73d019cba064642da229
Mark <mark@bhalash.com>
2015-02-11 12:57:28 +0000

    Changed archive article layout
    * Removed underline from title.
    * Removed “entire box” hyperlink.
    * Changed excerpt colour.
    * Changed excerpt to normal style.
    * Added category link as per full article style.

--- article-archive.php
+++ article-archive.php
@@ -1,14 +1,15 @@
 <article <?php post_class('archive-article'); ?> id="article-<?php the_ID(); ?>" style="background-image: url('<?php echo content_first_image(); ?>');">
     <div class="mask">
-        <a href="<?php the_permalink(); ?>" rel="bookmark">
-            <header>
-                <?php if (!is_page()) : ?>
-                    <p><small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time></small></p>
-                <?php endif; ?>
-                <h3><?php the_title(); ?></h3>
-            </header>
-            <?php printf('<p class="excerpt">"%s..."</p>', rmwb_excerpt()); ?>
-        </a>
+        <header>
+            <?php if (!is_page()) : ?>
+                <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); ?></small>
+            <?php endif; ?>
+            <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+        </header>
+        <div class="excerpt">
+            <?php the_excerpt(); ?>
+            <?php // printf('<p>"%s..."</p>', rmwb_excerpt()); ?>
+        </div>
         <footer>
             <small><?php edit_post_link('edit post', ' ', ''); ?></small>
         </footer>
