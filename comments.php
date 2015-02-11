bc272999a6a1d65111d9d1b6394529c0c10d7758
Mark <mark@bhalash.com>
2015-02-11 13:01:03 +0000

    Changed comment appearance
    * Fixed input wrapping in commentform inputs.
    * Added comment count indicator.

--- comments.php
+++ comments.php
@@ -2,14 +2,17 @@
     printf('<div class="article-comments" id="comments">');
 
     if (post_password_required()) {
-        printf('<h5>%s</h5>', 'This post is password protected. Enter the password to view comments.');
+        printf('<h5>%s</h5>', __('This post is password protected. Enter the password to view comments.'));
         return;
     }
 
     if (have_comments()) {
-        printf('<hr><h5>%s</h5>', get_comments_number('% comments.', '1 comment.', '% comments.'));
+        $s = (get_comments_number() == 1) ? '' : 's';
+
+        printf('<hr>');
+        printf(__('<h5>%d comment%s on \'%s\':</h5>'), get_comments_number(), $s, get_the_title());
         printf('<ul>');
-        
+
         wp_list_comments(array( 
             'callback' => 'rmwb_comments',
             'avatar_size' => 0,
@@ -20,15 +23,18 @@
         printf('</ul></div>');
     }
 
-    printf('<div id="comment-entry">');
+    printf('<hr><div id="comment-entry">');
+    printf('<h5>%s on \'%s\':</h5>', __('Have your own say'), get_the_title());
 
     comment_form(array(
-        'title_reply' => 'Have your say!',
+        'id_form' => 'commentform',
+        'id_submit' => 'submit',
+        'title_reply' => '',
         'comment_field' => '<p id="textarea"><textarea aria-required="true" class="comment-form-comment" id="comment" name="comment"></textarea></p>',
         'fields' => array(
-            'author' => '<input aria-required="true" class="author-name" id="author" name="author" placeholder="Name*" type="text" required>',
-            'email' => '<input aria-required="true" class="author-email" id="email" name="email" placeholder="Email*" type="text" required>',
-            'url' => '<input aria-required="false" class="author-url" id="url" name="url" placeholder="Website" type="text">'
+            'author' => '<input aria-required="true" class="author-name" id="comment-author" name="author" placeholder="Name*" type="text" required>',
+            'email' => '<input aria-required="true" class="author-email" id="comment-email" name="email" placeholder="Email*" type="text" required>',
+            'url' => '<input aria-required="false" class="author-url" id="comment-url" name="url" placeholder="Website" type="text">'
         )
     ));
 
