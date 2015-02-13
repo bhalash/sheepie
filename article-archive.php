64e47dc376f2eb3c5daa0ece933985884e1cccca
Mark <mark@bhalash.com>
2015-02-13 02:21:02 +0000

    Added title class to articles

--- article-archive.php
+++ article-archive.php
@@ -4,7 +4,7 @@
             <?php if (!is_page()) : ?>
                 <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); ?></small>
             <?php endif; ?>
-            <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+            <h3 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
         </header>
         <div class="excerpt">
             <?php the_excerpt(); ?>
--- article-full.php
+++ article-full.php
@@ -3,7 +3,7 @@
         <?php if (!is_page()) : ?>
             <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); ?></small>
         <?php endif; ?>
-        <h3><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+        <h3 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
     </header>
     <?php the_content('Read the rest of this post &raquo;'); ?>
     <footer>
