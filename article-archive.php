fb45afa204acc8e24d2ba34f6dc9845229718d3a
Mark <mark@bhalash.com>
2015-03-03 21:12:44 +0000

    Removed mask and background image from archive

--- article-archive.php
+++ article-archive.php
@@ -1,17 +1,15 @@
-<article <?php post_class('archive'); ?> id="article-<?php the_ID(); ?>" style="background-image: url('<?php echo content_first_image(); ?>');">
-    <div class="mask">
-        <header>
-            <?php if (!is_page()) : ?>
-                <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); ?></small>
-            <?php endif; ?>
-            <h3 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-        </header>
-        <div class="excerpt">
-            <?php the_excerpt(); ?>
-            <?php // printf('<p>"%s..."</p>', rmwb_excerpt()); ?>
-        </div>
-        <footer>
-            <small><?php edit_post_link('edit post', ' ', ''); ?></small>
-        </footer>
+<article <?php post_class('archive'); ?> id="article-<?php the_ID(); ?>">
+    <header>
+        <?php if (!is_page()) : ?>
+            <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); ?></small>
+        <?php endif; ?>
+        <h3 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+    </header>
+    <div class="excerpt">
+        <?php the_excerpt(); ?>
+        <?php // printf('<p>"%s..."</p>', rmwb_excerpt()); ?>
     </div>
+    <footer>
+        <small><?php edit_post_link('edit post', ' ', ''); ?></small>
+    </footer>
 </article>
