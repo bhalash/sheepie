4a027ddbbf4e9c4a6a1588b63fffa82e6924fc64
Mark <mark@bhalash.com>
2015-03-04 03:05:08 +0000

    Removed comments output

--- single.php
+++ single.php
@@ -4,12 +4,6 @@ if (have_posts()) {
     while (have_posts()) {
         the_post();
         get_template_part('article', 'full');
-
-        if (is_single()) {
-            get_template_part('related');
-        }
-        
-        comments_template();
     }
 } else {
     get_template_part('article', 'missing');
