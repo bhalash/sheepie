dce4f18dad7bd9e47e45e9aee83c69bdf9861ec1
Mark <mark@bhalash.com>
2015-03-04 03:06:31 +0000

    Refactored pagination
    Made it suitable for individual posts too.

--- pagination.php
+++ pagination.php
@@ -2,11 +2,22 @@
 $next = $paged + 1; 
 $previous = $paged - 1; ?>
 
-<div id="pagination">
-    <div class="previous">
-        <?php previous_posts_link('&larr; Page ' . $previous); ?>
-    </div>
-    <div class="next">
-        <?php next_posts_link('Page ' . $next . ' &rarr;'); ?>
-    </div>
-</div>
+<nav id="pagination">
+    <span class="previous">
+        <?php if (is_single()) {
+            next_post_link('&larr; %link');
+        } else {
+            previous_posts_link('&larr; Page ' . $previous);
+        } ?>
+    </span>
+    <?php if (!is_single()) : ?> 
+        <span class="count"><?php archive_page_count(); ?></span>
+    <?php endif; ?>
+    <span class="next">
+        <?php if (is_single()) {
+            previous_post_link('%link &rarr;');
+        } else {
+            next_posts_link('Page ' . $next . ' &rarr;'); 
+        } ?>
+    </span>
+</nav>
