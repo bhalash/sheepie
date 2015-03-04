59e836f518b7700029199fb31b5233f6e2ab4b7e
Mark <mark@bhalash.com>
2015-03-04 03:02:56 +0000

    Moved meta information to footer

--- article-archive.php
+++ article-archive.php
@@ -1,8 +1,5 @@
 <article <?php post_class('archive'); ?> id="article-<?php the_ID(); ?>">
     <header>
-        <?php if (!is_page()) : ?>
-            <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); ?></small>
-        <?php endif; ?>
         <h3 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
     </header>
     <div class="excerpt">
@@ -10,6 +7,8 @@
         <?php // printf('<p>"%s..."</p>', rmwb_excerpt()); ?>
     </div>
     <footer>
-        <small><?php edit_post_link('edit post', ' ', ''); ?></small>
+        <?php if (!is_page()) : ?>
+            <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); edit_post_link('edit post', ' / ', ''); ?></small>
+        <?php endif; ?>
     </footer>
 </article>
