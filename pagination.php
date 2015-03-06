6d89b9d11f071e0bf225704ecffc144a118ca09b
Mark <mark@bhalash.com>
2015-03-06 02:28:21 +0000

    Refactored pagination

--- pagination.php
+++ pagination.php
@@ -3,21 +3,21 @@ $next = $paged + 1;
 $previous = $paged - 1; ?>
 
 <nav id="pagination">
-    <span class="previous">
+    <div class="previous<?php echo (is_single()) ? '-post' : ''; ?>">
         <?php if (is_single()) {
             next_post_link('%link', '&larr; %title', false);
         } else {
             previous_posts_link('&larr; Page ' . $previous);
         } ?>
-    </span>
-    <?php if (!is_single()) : ?> 
-        <span class="count"><?php archive_page_count(); ?></span>
-    <?php endif; ?>
-    <span class="next">
+    </div>
+
+    <div class="count"><?php if (!is_single()) : ?><span><?php archive_page_count(); ?></span><?php endif; ?></div>
+
+    <div class="next<?php echo (is_single()) ? '-post' : ''; ?>">
         <?php if (is_single()) {
             previous_post_link('%link', '%title &rarr;', false);
         } else {
             next_posts_link('Page ' . $next . ' &rarr;'); 
         } ?>
-    </span>
+    </div>
 </nav>
