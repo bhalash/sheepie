73e8f342d712b3b235e4dc8bbced75dce0c2f404
Mark <mark@bhalash.com>
2015-03-22 02:12:10 +0000

    Returned pagination, comments and related posts to single posts

--- single.php
+++ single.php
@@ -4,6 +4,9 @@ if (have_posts()) {
     while (have_posts()) {
         the_post();
         get_template_part('article', 'full');
+        get_template_part('pagination');
+        get_template_part('related');
+        comments_template();
     }
 } else {
     get_template_part('article', 'missing');
