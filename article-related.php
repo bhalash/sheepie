89c7daa38448df5620f75aa0edaa449fa19251a0
Mark <mark@bhalash.com>
2015-03-22 02:18:11 +0000

    Changed placeholder name

--- article-related.php
+++ article-related.php
@@ -1,6 +1,6 @@
 <article <?php post_class('related'); ?> id="article-<?php the_ID(); ?>">
     <header>
-        <div class="foobar" style="background-image: url('<?php echo content_first_image(); ?>');">
+        <div class="related-thumbnail" style="background-image: url('<?php echo content_first_image(); ?>');">
             <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"></a>
         </div>
         <h5 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h5>
