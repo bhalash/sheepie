7da27e1ca92f76595f16a4f5cacbb99498324dc2
Mark <mark@bhalash.com>
2015-03-23 00:01:19 +0000

    Added author information to post footer

--- article-full.php
+++ article-full.php
@@ -8,4 +8,17 @@
         <?php endif; ?>
     </header>
     <?php the_content('Read the rest of this post &raquo;'); ?>
+    <?php if (is_single()) : ?>
+        <hr>
+        <footer>
+            <div class="avatar-wrapper">
+                <?php echo get_avatar(get_the_author_meta('ID'), 96); ?>
+            </div>
+            <div class="author-info">
+                <p><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); edit_post_link('edit post', ' / ', ''); ?></p>
+                <h4><?php the_author_meta('display_name'); ?></h4>
+                <p><?php the_author_meta('user_description'); ?></p>
+            </div>
+        </footer>
+    <?php endif; ?>
  </article>
