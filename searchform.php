19d4c22b0b133ba474a533ca4cbc0273f698f974
Mark <mark@bhalash.com>
2015-03-31 00:45:31 +0100

    Added search query to search input

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form class="search" action="<?php echo get_site_url(); ?>" method="get">
+<form class="search" action="<?php bloginfo('url'); ?>" method="get">
     <p>
-        <input type="text" class="search-text" name="s" tabindex="1" placeholder="Search the site" value="<?php echo get_search_query(); ?>" />
+        <input type="text" class="search-text" name="s" tabindex="1" placeholder="Search the site" value="<?php echo (is_search()) ? get_search_query() : ''; ?>" />
         <button type="submit"></button>
     </p>
 </form>
