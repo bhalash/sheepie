e6d9f6f09c945cf672859fd6518d84d7515e61fc
Mark <mark@bhalash.com>
2015-04-28 20:28:49 +0100

    Added GPL boilerplate

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,30 @@
+/**
+ * Miscellaneous JavaScript Functions
+ * -----------------------------------------------------------------------------
+ * @category   JavaScript File
+ * @package    Sheepie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2015 Mark Grealish
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU General Public License v3.0
+ * @version    3.0
+ * @link       https://github.com/bhalash/sheepie
+ *
+ * This file is part of Sheepie.
+ *
+ * Sheepie is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Sheepie is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * Sheepie. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 jQuery(function($) {
     'use strict';
     /*
