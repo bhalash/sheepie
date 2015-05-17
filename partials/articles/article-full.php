b8703bcd6be848d0f4fbad5bf06d2ae6389886e5
Mark <mark@bhalash.com>
2015-05-17 22:54:06 +0100

    Split off footer into its own file

--- partials/articles/article-full.php
+++ partials/articles/article-full.php
@@ -29,6 +29,7 @@
 ?>
 
 <article <?php post_class('full'); ?> id="article-<?php the_ID(); ?>">
+    
     <?php if (!is_single()) : ?>
         <header>
             <h3 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
@@ -36,19 +37,12 @@
                 <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); edit_post_link('edit post', ' / ', ''); ?></small>
             <?php endif; ?>
         </header>
-    <?php endif; ?>
-    <?php the_content('Read the rest of this post &raquo;'); ?>
-    <?php if (is_single()) : ?>
-        <hr>
-        <footer>
-            <div class="avatar-wrapper">
-                <?php echo get_avatar(get_the_author_meta('ID'), 96); ?>
-            </div>
-            <div class="author-info">
-                <p><small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); edit_post_link('edit post', ' / ', ''); ?></small></p>
-                <h4><?php the_author_meta('display_name'); ?></h4>
-                <p><?php the_author_meta('user_description'); ?></p>
-            </div>
-        </footer>
-    <?php endif; ?>
- </article>
+    <?php endif;
+
+    the_content('Read the rest of this post &raquo;');
+
+    if (is_single()) {
+        get_template_part(THEME_PARTIALS . '/articles/footer', 'full');
+    } ?>
+
+</article>
