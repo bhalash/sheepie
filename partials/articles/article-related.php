b4f872c74dc7282fe67bd7a2a1ddcc1c8f9c7400
Mark <mark@bhalash.com>
2015-05-17 22:53:52 +0100

    Changed header background output to existing function

--- partials/articles/article-related.php
+++ partials/articles/article-related.php
@@ -26,11 +26,12 @@
  * Sheepie. If not, see <http://www.gnu.org/licenses/>.
  */
 
+global $post;
 ?>
 
 <article <?php post_class('related'); ?> id="article-<?php the_ID(); ?>">
     <header>
-        <div class="related-thumbnail" style="background-image: url('<?php echo content_first_image(); ?>');">
+        <div class="related-thumbnail <?php header_class($post->ID); ?>" <?php header_background($post->ID); ?>>
             <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"></a>
         </div>
         <h5 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h5>
