344321777498721ea6464616c8e3c482f3b43821
Mark <mark@bhalash.com>
2015-05-17 22:51:48 +0100

    Separated article footer template

---
+++ partials/articles/footer-full.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Full Article Footer
+ * -----------------------------------------------------------------------------
+ * @category   PHP Script
+ * @package    Sheepie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2015 Mark Grealish
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU General Public License v3.0
+ * @version    3.0
+ * @link       https://github.com/bhalash/sheepie
+ *
+ * This file is part of Sheepie.
+ *
+ * Sheepie is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Sheepie is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * Sheepie. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
+
+<hr>
+<footer>
+    <div class="avatar-wrapper">
+        <?php printf(get_avatar(get_the_author_meta('ID'), 96)); ?>
+    </div>
+    <div class="author-info">
+        <p><small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); edit_post_link('edit post', ' / ', ''); ?></small></p>
+        <h4><?php the_author_meta('display_name'); ?></h4>
+        <p><?php the_author_meta('user_description'); ?></p>
+    </div>
+</footer>
