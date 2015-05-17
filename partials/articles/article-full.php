08b27f5d1de09429fe10b2fca3dea0201a78f8ae
Mark <mark@bhalash.com>
2015-05-17 17:50:32 +0100

    Changed scope of if loop

--- partials/articles/article-full.php
+++ partials/articles/article-full.php
@@ -29,14 +29,14 @@
 ?>
 
 <article <?php post_class('full'); ?> id="article-<?php the_ID(); ?>">
-    <header>
-        <?php if (!is_single()) : ?>
+    <?php if (!is_single()) : ?>
+        <header>
             <h3 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
             <?php if (!is_page()) : ?>
                 <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time> in <?php the_category(', '); edit_post_link('edit post', ' / ', ''); ?></small>
             <?php endif; ?>
-        <?php endif; ?>
-    </header>
+        </header>
+    <?php endif; ?>
     <?php the_content('Read the rest of this post &raquo;'); ?>
     <?php if (is_single()) : ?>
         <hr>
