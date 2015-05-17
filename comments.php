aa88b57257da49c192a26e929a281ff67c5a1e8d
Mark <mark@bhalash.com>
2015-05-18 00:51:52 +0100

    Added classes to headline items

--- comments.php
+++ comments.php
@@ -30,15 +30,15 @@ if (comments_open()) {
     printf('<div class="article-comments" id="comments">');
 
     if (post_password_required()) {
-        printf('<h5>%s</h5>', __('This post is password protected. Enter the password to view comments.'));
+        printf('<h5 class="reply-title">%s</h5>', __('This post is password protected. Enter the password to view comments.'));
         return;
     }
 
     if (have_comments()) {
-        $s = (get_comments_number() == 1) ? '' : 's';
+        $s = (get_comments_number() === 1) ? '' : 's';
 
         printf('<hr>');
-        printf(__('<h5>%d comment%s on \'%s\':</h5>'), get_comments_number(), $s, get_the_title());
+        printf(__('<h5 class="reply-title">%d comment%s on \'%s\':</h5>'), get_comments_number(), $s, get_the_title());
         printf('<ul>');
 
         wp_list_comments(array( 
@@ -52,12 +52,12 @@ if (comments_open()) {
     }
 
     printf('<hr><div id="comment-entry">');
-    printf('<h5>%s on \'%s\':</h5>', __('Have your own say'), get_the_title());
+    printf('<h5 class="reply-title">%s on \'%s\':</h5>', __('Have your own say'), get_the_title());
 
     comment_form(array(
         'id_form' => 'commentform',
         'id_submit' => 'submit',
-        'title_reply' => '',
+        'title_reply' => __('Have your say:'),
         'comment_field' => '<p id="textarea"><textarea autocomplete="off" class="comment-form-comment" id="comment" name="comment" required></textarea></p>',
         'comment_form_before_fields' => '<div class="argh">',
         'comment_form_after_fields' =>'</div>',
