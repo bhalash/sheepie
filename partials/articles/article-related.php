6060eb61a890244d55d9066755f175971bcdaf47
Mark <mark@bhalash.com>
2015-05-18 21:24:52 +0100

    Reduced headline size

--- partials/articles/article-related.php
+++ partials/articles/article-related.php
@@ -34,7 +34,7 @@ global $post;
         <div class="related-thumbnail <?php header_class($post->ID); ?>" <?php header_background($post->ID); ?>>
             <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"></a>
         </div>
-        <h5 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h5>
+        <h6 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h6>
         <?php if (!is_page()) : ?>
             <small><time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time(get_option('date_format')) ?></time></small>
         <?php endif; ?>
