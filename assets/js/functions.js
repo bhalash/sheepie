73711339736a747163b7f88a4b27377c1ef32661
Mark <mark@bhalash.com>
2015-05-18 01:15:23 +0100

    Removed defunct functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -24,14 +24,28 @@
  * Sheepie. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * Initialize highligh.js
+ * -----------------------------------------------------------------------------
+ */
+
 jQuery('pre').each(function() {
     if (jQuery(this).find('code').length === 0) {
         jQuery(this).wrapInner('<code></code>');
     }
 });
 
-// Initialize highlight.js ?>
 hljs.initHighlightingOnLoad();
+
+/*
+ * Comments Focus
+ * -----------------------------------------------------------------------------
+ */
+
+jQuery('.comment').click(function() { 
+    jQuery('.comment').not(this).removeClass('focused-comment');
+    jQuery(this).addClass('focused-comment');
+});
 
 // jQuery(function($) {
 //     'use strict';
@@ -85,115 +99,5 @@ hljs.initHighlightingOnLoad();
 //             click.preventDefault();
 //             window.location.href = search.url;
 //         }
-//     });
-
-//     /*
-//      * Commentform Toggle
-//      * ------------------
-//      * I've decided to hide the commentform unless the user chooses to use it.
-//      */
-
-//     $('#textarea textarea').focus(function() {
-//         $(this).css('height', 150);
-
-//         $('#commentform > *').slideDown(200, function() {
-//             $(window).scrollTop($('#commentform').offset().top);
-//         });
-//     });
-
-//     /*
-//      * Nav Box Shadow.
-//      * ---------------
-//      */
-
-//     var nav = {
-//         id: 'nav#top-menu',
-//         anchor: 'nav#top-menu li',
-//         content: '#content',
-//         shadow: 'nav-box-shadow',
-//         boxShadow: function() {
-//             // Set box shadow at bottom of nav menu on window scroll.
-//             if ($(window).scrollTop() === 0) {
-//                 $(nav.id).removeClass(nav.shadow);
-//             } else {
-//                 $(nav.id).addClass(nav.shadow);
-//             }
-//         },
-//         scrollOpacity: function() {
-//             // Change the opacity of the top nav menu on window scroll.
-//             var a = parseInt($(nav.content).css('padding-top')),
-//                 b = $(window).scrollTop(),
-//                 c = b / a;
-
-//             // Constrain opacity between 0 and 1.
-//             c = (c < 0) ? 0 : c;
-//             c = (c > 1) ? 1 : c;
-
-//             $(nav.id).css('background-color', 'rgba(255,255,255,' + c + ')');
-//         },
-//         invertTextColour: function() {
-//             /* Graduallly invert the colour of the anchor element as the page
-//              * scrolls, using RGB values. */
-
-//             var limits = {
-//                 /* The dark grey colour is 88,88,88, and white is 255,255,255
-//                  * The output RGB colour should be constrained between these. */ 
-//                 upper: 255,
-//                 lower: 88
-//             };
-
-//                 // Initial colour.
-//             var color = limits.upper, 
-//                 difference = limits.upper - limits.lower,
-//                 contentTop = parseInt($(nav.content).css('padding-top')),
-//                 windowPos = $(window).scrollTop(),
-//                 percentage = windowPos / contentTop;
-
-//             percentage = (percentage < 0) ? 0 : percentage;
-//             percentage = (percentage > 1) ? 1 : percentage;
-
-//             /* For example: 
-//              * color = 255(, 255, 2555)
-//              * difference = 255 - 80 = 167
-//              * percentange = (randomly) 0.46
-//              * 
-//              * color = 255 - (167 * 0.46) = 178 (rounded)
-//              */
-
-//             color = Math.floor(limits.upper - (difference * percentage));
-//             color = (color > limits.upper) ? limits.upper : color;
-//             color = (color < limits.lower) ? limits.lower : color;
-
-//             $(nav.anchor).css('color', 'rgba(' + color + ',' + color + ',' + color + ', 1)');
-//         }
-//     };
-
-//     $(window).on('scroll', nav.boxShadow);
-//     $(window).on('scroll', nav.invertTextColour);
-//     $(window).on('scroll', nav.scrollOpacity);
-
-//     /*
-//      * Header Size
-//      * -----------
-//      */
-
-//     if ($('body').hasClass('admin-bar')) {
-//         /* Push down the header to an appropriate degree if the WordPress admin
-//          * is displayed. */ 
-//         var a = $('#wpadminbar').height();
-//         $('nav#top-menu').css('top', a);
-//         $('#header').css('top', a);
-//     }
-
-//     $('nav#top-menu a[href$=search]').parent().css('float', 'right');
-
-//     /*
-//      * Comments Focus
-//      * --------------
-//      */
-
-//     $('.comment').click(function() { 
-//         $('.comment').not(this).removeClass('focused-comment');
-//         $(this).addClass('focused-comment');
 //     });
 // });
