c9443eecfe016dcc0535f9f86a17f54fb5299b6f
Mark <mark@bhalash.com>
2015-06-06 23:41:56 +0100

    Added placeholder date archive template

---
+++ date.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Dated Archive Template
+ * -----------------------------------------------------------------------------
+ * @category   PHP Script
+ * @package    Sheepie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2015 Mark Grealish
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU General Public License v3.0
+ * @version    3.0
+ * @link       https://github.com/bhalash/sheepie
+ *
+ * This file is part of Sheepie.
+ * 
+ * Sheepie is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Sheepie is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * Sheepie. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+get_header();
+global $paged;
+
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        get_template_part(THEME_ARTICLES . 'article', 'full');
+        printf('<hr>');
+    }
+} else {
+    get_template_part(THEME_ARTICLES . 'article', 'missing');
+}
+
+get_footer(); ?>
