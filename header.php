c33ad11f198f239815f1de64fd861a1fc6dfd9c2
Mark Grealish <mark@bhalash.com>
2015-08-01 14:37:37 +0000

    Cleaned up header

--- header.php
+++ header.php
@@ -31,14 +31,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <!-- The mystery of life isn't a problem to solve, but a reality to experience. -->
-    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
-    <title><?php wp_title('-', true, 'right'); ?></title>
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-    <?php wp_head(); ?>
+<!-- The mystery of life isn't a problem to solve, but a reality to experience. -->
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<title><?php wp_title('-', true, 'right'); ?></title>
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
     <div class="side-by-side" id="site">
