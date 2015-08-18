29137beeb6ae72e85f13a61a506fa74a2332d7b3
Mark Grealish <mark@bhalash.com>
2015-08-18 01:02:27 +0000

    Updated gulpfile to include IE build rules

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ var paths = {
         folder: assets.css,
         batch: assets.css + '*.scss',
         main: assets.css + 'main.scss',
+        ie: assets.css + 'ie.scss',
         out: assets.css
     },
     js: {
@@ -59,9 +60,20 @@ var prefixes = [
     'last 1 version', 
     '> 1%',
     'ie 10',
-    'ie 9',
-    'ie 8'
+    'ie 9'
 ];
+
+gulp.task('css', function() {
+    // Build CSS.
+    sass(paths.css.main, {
+            style: 'compressed'
+        })
+        .on('error', function(err) {
+            console.log(err.message);
+        })
+        .pipe(prefix(prefixes))
+        .pipe(gulp.dest(paths.css.out));
+});
 
 gulp.task('css-dev', function() {
     // Development and debug CSS.
@@ -77,15 +89,29 @@ gulp.task('css-dev', function() {
         .pipe(gulp.dest(paths.css.out));
 });
 
-gulp.task('css', function() {
-    // Build CSS.
-    sass(paths.css.main, {
+gulp.task('ie-css', function() {
+    // Build Internet Explorer CSS.
+    sass(paths.css.ie, {
             style: 'compressed'
         })
         .on('error', function(err) {
             console.log(err.message);
         })
         .pipe(prefix(prefixes))
+        .pipe(gulp.dest(paths.css.out));
+});
+
+gulp.task('ie-css-dev', function() {
+    // Development and debug CSS.
+    sass(paths.css.ie, {
+            sourcemap: true,
+            style: 'compressed'
+        })
+        .on('error', function(err) {
+            console.log(err.message);
+        })
+        .pipe(prefix(prefixes))
+        .pipe(sourcemap.write())
         .pipe(gulp.dest(paths.css.out));
 });
 
@@ -102,8 +128,10 @@ gulp.task('js', function() {
 gulp.task('default', function() {
     gulp.watch(paths.js.batch, ['js']);
     gulp.watch(paths.css.batch, ['css']);
+    gulp.watch(paths.css.ie, ['ie-css']);
 });
 
 gulp.task('css-dev-watch', function() {
     gulp.watch(paths.css.batch, ['css-dev']);
+    gulp.watch(paths.css.ie, ['ie-css-dev']);
 });
