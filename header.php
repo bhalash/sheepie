10d98f252e3fbdb0b1a16c3738dd0d0c56a877af
Mark Grealish <mark@bhalash.com>
2015-08-21 12:54:37 +0000

    Removed excessive GPL boilerplate from files

--- header.php
+++ header.php
@@ -7,23 +7,9 @@
  * @package    Sheepie
  * @author     Mark Grealish <mark@bhalash.com>
  * @copyright  Copyright (c) 2015 Mark Grealish
- * @license    https://www.gnu.org/copyleft/gpl.html The GNU General Public License v3.0
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU GPL v3.0
  * @version    3.0
  * @link       https://github.com/bhalash/sheepie
- *
- * This file is part of Sheepie.
- * 
- * Sheepie is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software 
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- * 
- * Sheepie is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
- * Sheepie. If not, see <http://www.gnu.org/licenses/>.
  */
 
 global $post;
@@ -43,26 +29,25 @@ global $post;
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <div class="side-by-side" id="site">
-        <div class="<?php header_class($post->ID); ?>" id="header" <?php post_image_css($post->ID, true); ?>>
-            <?php // 1. Output a pretty background image, if it exists. ?>
-            <div id="header-content">
-                <h2 id="header-title">
-                    <a title="<?php bloginfo('name'); ?>" href="<?php printf(home_url()); ?>"><?php bloginfo('name'); ?></a>
-                </h2>
+    <div class="<?php header_class($post->ID); ?>" id="header">
+        <?php // 1. Output a pretty background image, if it exists. ?>
+        <div class="header-content">
+            <h2 class="header-title">
+                <a title="<?php bloginfo('name'); ?>" href="<?php printf(home_url()); ?>"><?php bloginfo('name'); ?></a>
+            </h2>
 
-                <p id="header-description">
-                    <?php // 2. Either site information or post meta content. ?>
-                    <span>
-                        <?php bloginfo('description'); ?>
-                    </span>
-                </p>
+            <p class="header-description">
+                <?php // 2. Either site information or post meta content. ?>
+                <span>
+                    <?php bloginfo('description'); ?>
+                </span>
+            </p>
 
-                <div id="header-social">
-                    <?php // 3. Social navigation menu. ?>
-                    <?php wp_nav_menu(array('theme_location' => 'top-social')); ?>
-                </div>
+            <div class="header-social">
+                <?php // 3. Social navigation menu. ?>
+                <?php wp_nav_menu(array('theme_location' => 'top-social')); ?>
             </div>
         </div>
-        <div id="content">
-            <div id="interior">
+    </div>
+    <div id="content">
+        <div id="interior">
