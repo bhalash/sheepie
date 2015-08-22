03c38ddaf924b3c129a807168e1af426da0d3e38
Mark Grealish <mark@bhalash.com>
2015-08-22 21:35:10 +0000

    Aggregated scripts

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -12,7 +12,6 @@
 
 
 (function($) {
-
     /*
      * Initialize highligh.js
      * -------------------------------------------------------------------------
@@ -32,12 +31,21 @@
 
     $('.article-photobox br').remove();
 
+    /**
+     * IE9 Placeholder Polyfill
+     * -------------------------------------------------------------------------
+     */
+
+    if ($.fn.placeholder) {
+        $('input, textarea').placeholder();
+    }
+
     /*
      * Initialize Lightbox
      * -------------------------------------------------------------------------
      */
 
-    jQuery('article a img').addLightbox();
+    $('article a img').addLightbox();
 
     /**
      * Bigsearch Toggle 
@@ -66,6 +74,22 @@
 
     $('.bigsearch-toggle').bigSearchToggle('#bigsearch');
 })(jQuery, window, document, undefined);
+
+/**
+ * Google Analytics
+ * -----------------------------------------------------------------------------
+ */
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-2156640-5']);
+_gaq.push(['_setDomainName', 'bhalash.com']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 /*
  * Placeholder Fallback IE9
