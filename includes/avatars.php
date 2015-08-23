c074c1ee4ac4327d1648c1dfe7d87a4328e5ed7b
Mark Grealish <mark@bhalash.com>
2015-08-23 15:38:53 +0000

    Removed switch that prevented comment avatars from displaying

--- includes/avatars.php
+++ includes/avatars.php
@@ -55,10 +55,6 @@ function get_avatar_url_only($avatar, $id_or_email, $size, $default, $alt) {
  */
 
 function get_avatar_background($id_or_email, $size = 'large', $classes = null) {
-    if (!($author = get_user_by('id', $id_or_email))) {
-        return false;
-    }
-
     $avatar = get_avatar($id_or_email, $size);
     $background = sprintf('background-image: url(%s);', $avatar);
 
