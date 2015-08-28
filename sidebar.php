bc1b04aab6ccd87913aa741c154aa25a747ab325
Mark Grealish <mark@bhalash.com>
2015-08-28 14:24:59 +0000

    Added sidebar widget call

---
+++ sidebar.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Template Sidebar
+ * -----------------------------------------------------------------------------
+ * @category   PHP Script
+ * @package    Sheepie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2015 Mark Grealish
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU GPL v3.0
+ * @version    3.0
+ * @link       https://github.com/bhalash/sheepie
+ */
+
+if (is_active_sidebar('theme-widgets')) {
+    printf('<hr>');
+    printf('<div id="%s">', 'widgets');
+    dynamic_sidebar('theme-widgets');
+    printf('</div>');
+}
+
+?>
