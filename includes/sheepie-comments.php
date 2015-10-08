747433d333fee53bd1f59cede0b36292c3965739
Mark Grealish <mark@bhalash.com>
2015-10-08 12:12:40 +0000

    Changed fieldset back to div because it does not play nice with flexbox

--- includes/sheepie-comments.php
+++ includes/sheepie-comments.php
@@ -78,11 +78,11 @@ function sheepie_theme_comments($comment, $args, $depth) {
  */
 
 function sheepie_wrap_comment_fields_before() {
-    printf('<fieldset class="commentform-inputs">');
+    printf('<div class="commentform-inputs">');
 }
 
 function sheepie_wrap_comment_fields_after() {
-    printf('</fieldset>');
+    printf('</div>');
 }
 
 add_action('comment_form_before_fields', 'sheepie_wrap_comment_fields_before');
