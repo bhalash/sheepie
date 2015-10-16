a92802a02df563b901c7461b5b1957deacd1fe8a
Mark Grealish <mark@bhalash.com>
2015-10-16 20:51:14 +0000

    Changed all header and search classes to BEM style

--- partials/head.php
+++ partials/head.php
@@ -23,21 +23,21 @@ $action = esc_url(home_url('/'));
 
 ?>
 
-<header id="navbar">
-    <div class="navbar-head">
-        <div class="navbar-head-content">
-            <div class="navbar-title navbar-child">
-                <h2 class="site-name">
+<header class="navbar" id="navbar">
+    <div class="navbar__contentwrap color--rmwb--bg">
+        <div class="navbar__content">
+            <div class="navbar__child navbar__titlewrapper">
+                <h2 class="navbar__title">
                     <?php printf('<a class="%s" href="%s">%s</a>',
-                        'navbar-site-name',
+                        'navbar__title-link',
                         home_url(),
                         get_bloginfo('name')
                     ); ?>
                 </h2>
             </div>
 
-            <div class="navbar-description navbar-child">
-                <span>
+            <div class="navbar__description navbar__child">
+                <span class="font--small">
                     <?php bloginfo('description'); ?>
                 </span>
             </div>
@@ -45,25 +45,27 @@ $action = esc_url(home_url('/'));
             <?php wp_nav_menu(array(
                 'theme_location' => 'top-social',
                 'container' => 'div',
-                'container_class' => 'navbar-socialrow navbar-child'
+                'container_class' => 'navbar__social navbar__child'
             )); ?>
 
-            <div class="navbar-buttonrow navbar-child">
-                <button id="navrow-search-toggle" class="bigsearch-toggle toggle">
-                    <span class="toggle-icon search"></span> 
+            <div class="navbar__buttonrow navbar__child">
+                <button class="navbar__button button button--search-navbar toggle bigsearch__toggle" id="searchtoggle__navbar">
+                    <span class="button__icon search"></span> 
                 </button>
             </div>
         </div>
     </div>
-    <div class="disp--hidden" id="bigsearch">
-        <form class="bigsearch-form" method="get" action="<?php printf($action); ?>" autocomplete="off" novalidate>
-            <fieldset>
-                <input class="bigsearch-input" id="bigsearch-input" name="s" type="search" placeholder="<?php _e('search', 'sheepie'); ?>" required="required">
-                <label class="bigsearch-label" for="bigsearch-input"><?php _e('search', 'sheepie'); ?></label>
-            </fieldset>
-        </form>
-        <button class="bigsearch-toggle toggle" id="bigsearch-search-toggle">
-            <span class="toggle-icon search"></span> 
-        </button>
-    </div>
 </header>
+
+<div class="disp--hidden bigsearch color--rmwb--bg" id="bigsearch">
+    <form class="bigsearch__form" method="get" action="<?php printf($action); ?>" autocomplete="off" novalidate>
+        <fieldset>
+            <input class="bigsearch__input" id="bigsearch__input" name="s" type="search" placeholder="<?php _e('search', 'sheepie'); ?>" required="required">
+            <label class="bigsearch__label" for="bigsearch__input"><?php _e('search', 'sheepie'); ?></label>
+        </fieldset>
+    </form>
+
+    <button class="button button--search-bigsearch bigsearch__toggle" id="searchtoggle__search">
+        <span class="button__icon search"></span> 
+    </button>
+</div>
