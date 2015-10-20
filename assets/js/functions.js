34ce086bd2f66bf5277207fa159c149e8e80c220
Mark Grealish <mark@bhalash.com>
2015-10-20 19:57:42 +0000

    Site JavaScript refactor
    * Moved jQuery functions->Knockout.
    * Replaced old lightbox jQuery with Knockout.
    * Replaced old search jQuery with Knockout.
    * Replaced <br /> removal with vanilla JS.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -12,47 +12,134 @@
 
 ;(function($) {
     /*
-     * Initialize highlight.js
+     * Wrap Pre Elements
      * -------------------------------------------------------------------------
      * highlight.js operates on the <code> child or <pre> elements. This wraps the
      * content of an element with <code> before initializing highlight.js.
+     *
+     * TODO: replace with vanilla JS.
      */
 
     if ($('pre').length) {
         $('pre:not(:has(> code))').wrapInner('<code></code>');
     }
 
-    hljs.initHighlightingOnLoad();
-
     /**
      * Remove Photobox Breaks
      * -------------------------------------------------------------------------
      * WordPress can insert <br> tags between elements if it detects either a
-     * space or a line break. This can break the formatting of the lightbox.
+     * space or a line break. This can break the formatting. Runs once.
      */
 
-    $('.article-photobox br').remove();
+    var pb = document.querySelectorAll('[class^=article-photobox] br');
 
-    /*
-     * Initialize Lightbox
-     * -------------------------------------------------------------------------
-     */
+    if (pb.length) {
+        [].forEach.call(pb, function(e) {
+            e.parentNode.removeChild(e);
+        });
+    }
 
-    $('article a img').addLightbox();
-
-    /*
-     * Linked Class Toggles
-     * -------------------------------------------------------------------------
-     */
-
-    $('.bigsearch__toggle').link({
-        child: '.button__icon',
-        childClass: 'close',
-        target: '#bigsearch',
-        targetClass: 'disp--shown',
-        isTargetInput: true,
-    });
 })(jQuery, window, document, undefined);
+
+;(function() {
+    /*
+     * Lightbox and Search Controller
+     * -------------------------------------------------------------------------
+     *  Built around Knockout.js, this controller manages the search and
+     *  lightbox display on the site, in conjunction with a tiny PHP function
+     *  to add the click directive to article images.
+     */
+
+    var sheepieController = function() {
+        var self = this;
+
+        /*
+         * View States
+         * ---------------------------------------------------------------------
+         *  Add whatever extra states here.
+         */
+
+        self.display = {
+            search: ko.observable(false),
+            lightbox: ko.observable(false)
+        };
+
+        /*
+         * Lightbox Attributes
+         * ---------------------------------------------------------------------
+         * Pulled from the bound image. Link isn't used, although it is useful
+         * for future use.
+         */
+
+        self.lightbox = {
+            text: ko.observable(null),
+            image: ko.observable(null),
+            link: ko.observable(null)
+        };
+
+        /*
+         * Change State
+         * ---------------------------------------------------------------------
+         *  1. Hide all the eiements, except name.
+         *  2. If name exists, invert its state.
+         *
+         * @param   string      name        State to toggle true.
+         */
+
+        self.show = function(name) {
+            for (var i in self.display) {
+                if (name && name in self.display && self.display[i]() === self.display[name]()) {
+                    continue;
+                }
+
+                self.display[i](false);
+            }
+
+            if (name && name in self.display) {
+                self.display[name](!self.display[name]());
+            }
+        };
+
+        /*
+         * Set State to: Whatever
+         * ---------------------------------------------------------------------
+         */
+
+        self.toggleLightbox = function() {
+            self.show('lightbox');
+        };
+
+        self.toggleSearch = function() {
+            self.show('search');
+        };
+
+        /*
+         * Close Lightbox/Search/Whatever on Escape
+         * ---------------------------------------------------------------------
+         */
+
+        self.closeOnEscape = function(data, event) {
+            if (event.keyCode === 27) {
+                self.show(null);
+            }
+        };
+
+        /*
+         * Set Lightbox Data and Open Lightbox
+         * ---------------------------------------------------------------------
+         */
+
+        self.showLightbox = function(data, event) {
+            self.lightbox.text(event.target.attributes.alt.value);
+            self.lightbox.image(event.target.attributes.src.value);
+            self.lightbox.link(event.target.parentNode.attributes.href.value);
+
+            self.show('lightbox');
+        };
+    };
+
+    ko.applyBindings(new sheepieController());
+})();
 
 /**
  * Google Analytics
