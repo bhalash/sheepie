19e71cc457805280840937e13fbb9afa24144320
Mark Grealish <mark@bhalash.com>
2015-10-22 09:40:45 +0000

    Fixed typo in query var name

--- partials/pagination-site.php
+++ partials/pagination-site.php
@@ -12,15 +12,15 @@
  * @link       https://github.com/bhalash/sheepie
  */
 
-$pages = (get_query_var('pages')) ? get_query_var('pages') : 1;
-$next = $pages + 1; 
-$previous = $pages - 1; 
+$page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$page_next = $page + 1; 
+$page_previous = $page - 1; 
 
 ?>
 
 <nav class="pagination pagination--site" id="pagination--site">
     <p class="pagination__previous previous-page">
-        <span class="font--small"><?php previous_posts_link(__('&larr; Page ', 'sheepie') . $previous); ?></span>
+        <span class="font--small"><?php previous_posts_link(__('&larr; Page ', 'sheepie') . $page_previous); ?></span>
     </p>
     <?php if (function_exists('arc_query_has_pages') && arc_query_has_pages()) : ?>
         <p class="pagination__count">
@@ -28,6 +28,6 @@ $previous = $pages - 1;
         </p>
     <?php endif; ?>
     <p class="pagination__next next-page">
-        <span class="font--small"><?php next_posts_link(__('Page ', 'sheepie') . $next . ' &rarr;');  ?></span>
+        <span class="font--small"><?php next_posts_link(__('Page ', 'sheepie') . $page_next. ' &rarr;');  ?></span>
     </p>
 </nav>
