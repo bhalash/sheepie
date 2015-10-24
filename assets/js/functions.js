2dbae892899b5e0079cf5421eb99f7f5b097f8fb
Mark Grealish <mark@bhalash.com>
2015-10-24 11:38:54 +0000

    The no-more-jQuery commit
    * Replaced last jQuery-dependent actions with vanilla JS.
    * Moved Knockout JS actions to within closures.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,38 +10,61 @@
  * @link       https://github.com/bhalash/sheepie
  */
 
-;(function($) {
+;(function() {
     /*
      * Wrap Pre Elements
      * -------------------------------------------------------------------------
      * highlight.js operates on the <code> child or <pre> elements. This wraps the
      * content of an element with <code> before initializing highlight.js.
      *
-     * TODO: replace with vanilla JS.
+     * This does the same thing as jQuery's $(foo).wrapInner(), albeit with less
+     * regex selector magic.
+     *
+     *  if ($('pre').length) {
+     *      $('pre:not(:has(> code))').wrapInner('<code></code>');
+     *  } 
      */
 
-    if ($('pre').length) {
-        $('pre:not(:has(> code))').wrapInner('<code></code>');
+    function wrapInsideElement(selector, wrapper) {
+        selector = document.querySelectorAll(selector);
+        wrapper = wrapper.replace(/(^<.*\/|>$)/g, '');
+
+        [].forEach.call(selector, function(element) {
+            if (element.querySelectorAll(wrapper).length === 0) {
+                var newChild = document.createElement(wrapper);
+                
+                while (element.childNodes.length) {
+                    newChild.appendChild(element.childNodes[0]);
+                }
+
+                element.appendChild(newChild);
+            }
+        });
     }
+
+    wrapInsideElement('pre', '<code></code>');
 
     /**
      * Remove Photobox Breaks
      * -------------------------------------------------------------------------
      * WordPress can insert <br> tags between elements if it detects either a
      * space or a line break. This can break the formatting. Runs once.
+     *
+     * Does exactly the same thing as jQuery's $(foo).remove().
      */
 
-    var pb = document.querySelectorAll('[class^=article-photobox] br');
+    function removeSelector(selector) {
+        selector = document.querySelectorAll(selector);
 
-    if (pb.length) {
-        [].forEach.call(pb, function(e) {
-            e.parentNode.removeChild(e);
-        });
+        if (selector.length) {
+            [].forEach.call(selector, function(element) {
+                element.parentNode.removeChild(element);
+            });
+        }
     }
 
-})(jQuery, window, document, undefined);
+    removeSelector('[class^=article-photobox] br');
 
-;(function() {
     /*
      * Lightbox and Search Controller
      * -------------------------------------------------------------------------
@@ -51,92 +74,101 @@
      */
 
     var sheepieController = function() {
-        var self = this;
+        return function() {
+            var self = this;
 
-        /*
-         * View States
-         * ---------------------------------------------------------------------
-         *  Add whatever extra states here.
-         */
+            /*
+             * View States
+             * ---------------------------------------------------------------------
+             *  Add whatever extra states here.
+             */
 
-        self.display = {
-            search: ko.observable(false),
-            lightbox: ko.observable(false)
-        };
+            self.display = {
+                search: ko.observable(false),
+                lightbox: ko.observable(false)
+            };
 
-        /*
-         * Lightbox Attributes
-         * ---------------------------------------------------------------------
-         * Pulled from the bound image. Link isn't used, although it is useful
-         * for future use.
-         */
+            /*
+             * Lightbox Attributes
+             * ---------------------------------------------------------------------
+             * Pulled from the bound image. Link isn't used, although it is useful
+             * for future use.
+             */
 
-        self.lightbox = {
-            text: ko.observable(null),
-            image: ko.observable(null),
-            link: ko.observable(null)
-        };
+            self.lightbox = {
+                text: ko.observable(null),
+                image: ko.observable(null),
+                link: ko.observable(null)
+            };
 
-        /*
-         * Change State
-         * ---------------------------------------------------------------------
-         *  1. Hide all the eiements, except name.
-         *  2. If name exists, invert its state.
-         *
-         * @param   string      name        State to toggle true.
-         */
+            /*
+             * Change State
+             * ---------------------------------------------------------------------
+             * The modal (search, lightbox) element on this site have an exclusive 
+             * appearance: only one at a time should display.
+             *
+             *  1. Hide all the eiements, except name.
+             *  2. If name exists, invert its state.
+             *
+             * @param   string      name        State to toggle true.
+             */
 
-        self.show = function(name) {
-            for (var i in self.display) {
-                if (name && name in self.display && self.display[i]() === self.display[name]()) {
-                    continue;
+            self.show = function(name) {
+                for (var i in self.display) {
+                    if (name && name in self.display && self.display[i]() === self.display[name]()) {
+                        continue;
+                    }
+
+                    self.display[i](false);
                 }
 
-                self.display[i](false);
-            }
+                if (name && name in self.display) {
+                    self.display[name](!self.display[name]());
+                }
+            };
 
-            if (name && name in self.display) {
-                self.display[name](!self.display[name]());
-            }
-        };
+            /*
+             * Set State to: Whatever
+             * ---------------------------------------------------------------------
+             */
 
-        /*
-         * Set State to: Whatever
-         * ---------------------------------------------------------------------
-         */
+            self.toggleLightbox = function() {
+                self.show('lightbox');
+            };
 
-        self.toggleLightbox = function() {
-            self.show('lightbox');
-        };
+            self.toggleSearch = function() {
+                self.show('search');
+            };
 
-        self.toggleSearch = function() {
-            self.show('search');
-        };
+            /*
+             * Close Lightbox/Search/Whatever on Escape
+             * ---------------------------------------------------------------------
+             *  @param  object      data        Data passed.
+             *  @param  object      event       Event and element information.
+             */
 
-        /*
-         * Close Lightbox/Search/Whatever on Escape
-         * ---------------------------------------------------------------------
-         */
+            self.closeOnEscape = function(data, event) {
+                if (event.keyCode === 27) {
+                    self.show(null);
+                }
+            };
 
-        self.closeOnEscape = function(data, event) {
-            if (event.keyCode === 27) {
-                self.show(null);
-            }
-        };
+            /*
+             * Set Lightbox Data and Open Lightbox
+             * ---------------------------------------------------------------------
+             *  @param  object      data        Data passed.
+             *  @param  object      event       Event and element information.
+             */
 
-        /*
-         * Set Lightbox Data and Open Lightbox
-         * ---------------------------------------------------------------------
-         */
+            self.showLightbox = function(data, event) {
+                self.lightbox.text(event.target.attributes.alt.value);
+                self.lightbox.image(event.target.attributes.src.value);
+                self.lightbox.link(event.target.parentNode.attributes.href.value);
 
-        self.showLightbox = function(data, event) {
-            self.lightbox.text(event.target.attributes.alt.value);
-            self.lightbox.image(event.target.attributes.src.value);
-            self.lightbox.link(event.target.parentNode.attributes.href.value);
-
-            self.show('lightbox');
-        };
-    };
+                self.show('lightbox');
+            };
+        }
+    }
 
     ko.applyBindings(new sheepieController());
 })();
