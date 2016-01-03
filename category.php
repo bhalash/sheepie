564d5d4a188262f2d76aa8f4a2cf34870359a628
Mark Grealish <mark@bhalash.com>
2016-01-03 00:06:53 +0000

    Added category archive template.

---
+++ category.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Category Archive Template
+ * -----------------------------------------------------------------------------
+ * @category   PHP Script
+ * @package    Sheepie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2015 Mark Grealish
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU GPL v3.0
+ * @version    1.0
+ * @link       https://github.com/bhalash/sheepie
+ */
+
+$category = get_category(get_query_var('cat'));
+
+get_header();
+sheepie_partial('gohome');
+
+printf('<h2 class="%s"><a href="%s">%s</a></h2>',
+    'title vspace--double',
+    get_category_link($category),
+    $category->name
+);
+
+$category_posts = get_posts(array(
+    'category' => $category->cat_ID,
+    'posts_per_page' => 30,
+    'orderby' => 'date',
+));
+
+foreach ($category_posts as $post) {
+    setup_postdata($post);
+    sheepie_partial('article', 'excerpt');
+}
+
+wp_reset_query();
+
+printf('<hr class="%s">', 'vcenter--triple');
+sheepie_partial('pagination', 'site');
+get_footer();
+
+?>
