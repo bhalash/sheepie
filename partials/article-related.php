b1eec2e17909291be6a3a450c0dbb26baf79da40
Mark Grealish <mark@bhalash.com>
2016-01-20 19:19:58 +0000

    Fixed indentation of line.

--- partials/article-related.php
+++ partials/article-related.php
@@ -19,7 +19,7 @@ $post_classes = array('article', 'article--related', 'vspace--half');
 <article <?php post_class($post_classes); ?> id="article--related--<?php the_ID(); ?>">
     <a class="article--related__link" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
         <header class="article--related__header vspace--half">
-                <div class="thumbnail article--related__thumbnail" <?php post_image_css($post->ID, true); ?>></div>
+            <div class="thumbnail article--related__thumbnail" <?php post_image_css($post->ID, true); ?>></div>
         </header>
         <h4 class="title article--related__title vspace--quarter">
             <?php the_title(); ?>
