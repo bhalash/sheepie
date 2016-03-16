09e4e715c38b1f34c6c91958a01b44049a2eb573
Mark Grealish <mark@bhalash.com>
2016-03-16 21:59:31 +0000

    Split header partials into separate files.

---
+++ partials/header-bigsearch.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Main Search Form
+ * -----------------------------------------------------------------------------
+ * I separated this template because of the 404 switch. It was easier to wrap it
+ * all up here.
+ *
+ * @category   PHP Script
+ * @package    Sheepie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2015 Mark Grealish
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU GPL v3.0
+ * @version    1.0
+ * @link       https://github.com/bhalash/sheepie
+ */
+
+?>
+
+<div class="disp--hidden bigsearch color--rmwb--bg noprint" id="bigsearch" data-bind="css: {'disp--hidden': !elements.search()}">
+    <form class="bigsearch__form" method="get" action="<?php printf($action); ?>" autocomplete="off" novalidate>
+        <fieldset>
+            <input class="bigsearch__input" id="bigsearch__input" name="s" type="search" placeholder="<?php _e('search', 'sheepie'); ?>" required="required" data-bind="hasfocus: elements.search()">
+            <label class="bigsearch__label" for="bigsearch__input"><?php _e('search', 'sheepie'); ?></label>
+        </fieldset>
+    </form>
+
+    <button class="button button--search-bigsearch bigsearch__toggle social search" id="searchtoggle__search" data-bind="css: {close: elements.search()}, click: toggleSearch">
+        <span class="button__icon social__icon" data-bind=""></span>
+    </button>
+</div>
---
+++ partials/header-lightbox.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Lightbox
+ * -----------------------------------------------------------------------------
+ * I separated this template because of the 404 switch. It was easier to wrap it
+ * all up here.
+ *
+ * @category   PHP Script
+ * @package    Sheepie
+ * @author     Mark Grealish <mark@bhalash.com>
+ * @copyright  Copyright (c) 2015 Mark Grealish
+ * @license    https://www.gnu.org/copyleft/gpl.html The GNU GPL v3.0
+ * @version    1.0
+ * @link       https://github.com/bhalash/sheepie
+ */
+
+?>
+
+<div class="disp--hidden lightbox noprint" id="lightbox" data-bind="css: { 'disp--hidden': !elements.lightbox() }">
+    <a class="lightbox__anchor" href="#!" data-bind="click: show, attr: {title: lightbox.text, href: lightbox.link}">
+        <img class="lightbox__image" data-bind="attr: {src: lightbox.image, alt: lightbox.text}" />
+    </a>
+</div>
