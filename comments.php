017af25f439558670f9c94b84edf65ceb1c7c067
Mark Grealish <mark@bhalash.com>
2016-03-18 13:34:08 +0000

    Refactored comment output for legibility.

--- comments.php
+++ comments.php
@@ -12,68 +12,56 @@
  * @link       https://github.com/bhalash/sheepie
  */
 
-if (comments_open()) {
-    if (post_password_required()) {
-        return;
-    }
+if (!comments_open() || post_password_required()) {
+    return;
+}
 
-    printf('<hr class="%s">', 'vcenter--double');
+// Template input for name, email and URL.
+$input_html = '<input class="comments__input %s-name font-size--small" id="%s" name="%s" placeholder="%s" type="text" required="required">';
+$textarea_html = '<textarea class="comments__textarea" id="comment" name="comment" required="required"></textarea>';
 
-    printf('<div class="comments" id="comments">');
-        printf('<h4 class="%s">%s \'%s\'</h4>',
-            'vspace--full subtitle noprint',
-            __('Have your say on ', 'sheepie'),
-            get_the_title()
-        );
+$input_fields = [
+    'author' => __('Name*', 'sheepie'),
+    'email' => __('Email*', 'sheepie'),
+    'url' => __('Website', 'sheepie')
+];
 
-    if (have_comments()) {
-        printf('<ul class="%s">', 'comments__commentlist');
-
-        wp_list_comments([
-            'callback' => 'sheepie_theme_comments',
-            'avatar_size' => 0,
-            'format' => 'html5',
-            'style' => 'ul'
-        ]);
-
-        printf('</ul>');
-    }
-
-    if (get_comment_pages_count() > 1) {
-        sheepie_partial('pagination', 'comment');
-    }
-
-    printf('<div class="noprint" id="comments__entry">');
-
-    // Template input for name, email and URL.
-    $input = '<input class="comments__input %s-name font-size--small" id="%s" name="%s" placeholder="%s" type="text" required="required">';
-    $textarea = '<textarea class="comments__textarea" id="comment" name="comment" required="required"></textarea>';
-
-    $fields = [
-        // Name, author and email fields.
-        'author' => sprintf($input,
-            'author', 'author', 'author', __('Name*', 'sheepie')
-        ),
-        'email' => sprintf($input,
-            'email', 'email', 'email', __('Email*', 'sheepie')
-        ),
-        'url' => sprintf($input,
-            'url', 'url', 'url', __('Website', 'sheepie')
-        )
-    ];
-
-    comment_form([
-        'id_form' => 'comments__form',
-        'id_submit' => 'comments__submit',
-        'title_reply' => '',
-        'comment_field' => sprintf('<p id="textarea">%s</p>', $textarea),
-        'comment_form_before_fields' => '<div class="comments__form">',
-        'comment_form_after_fields' => '</div>',
-        'fields' => $fields
-    ]);
-
-    printf('</div>');
-    printf('</div>');
+foreach ($input_fields as $field => $label) {
+    $input_fields[$field] = sprintf($input_html, $field, $field, $field, $label);
 }
 
 ?>
+
+<hr class="vcenter--double">
+<div class="comments" id="comments">
+    <h4 class="vspace--full subtitle noprint">
+        <?php _e('Have your say on ', 'sheepie'); the_title(); ?>
+    </h4>
+
+    <?php if (have_comments()) : ?>
+        <ul class="comments__commentlist">
+            <?php wp_list_comments([
+                'callback' => 'sheepie_theme_comments',
+                'avatar_size' => 0,
+                'format' => 'html5',
+                'style' => 'ul'
+            ]); ?>
+        </ul>
+    <?php endif; ?>
+
+    <?php if (get_comment_pages_count() > 1) {
+        sheepie_partial('pagination', 'comment');
+    } ?>
+
+    <div class="noprint" id="comments__entry">
+        <?php comment_form([
+            'id_form' => 'comments__form',
+            'id_submit' => 'comments__submit',
+            'title_reply' => '',
+            'comment_field' => sprintf('<p id="textarea">%s</p>', $textarea_html),
+            'comment_form_before_fields' => '<div class="comments__form">',
+            'comment_form_after_fields' => '</div>',
+            'fields' => $input_fields
+        ]); ?>
+    </div>
+</div>
