397cb5f79f79b51a44a39fd2731a5225e9970e2d
Mark Grealish <mark@bhalash.com>
2016-04-17 15:52:26 +0000

    Layout tweaks.

--- partials/article-full.php
+++ partials/article-full.php
@@ -12,19 +12,16 @@
  * @link       https://github.com/bhalash/sheepie
  */
 
-$author = get_the_author_meta('ID');
-$post_classes = array('article', 'article--full', 'vspace--half');
-
 ?>
 
-<article <?php post_class($post_classes); ?> id="article--full--<?php the_ID(); ?>">
-    <header class="article--full__header vspace--full">
-        <?php if (!is_page()) : ?>
-            <h6 class="article--meta font--small vspace--half noprint"><?php sheepie_postmeta(); ?></h6>
-        <?php endif; ?>
-        <h2 class="title article--full__title">
+<article <?php post_class(['article', 'article--full', 'vspace--double']); ?> id="article--full--<?php the_ID(); ?>">
+    <header class="article--full__header vspace--double">
+        <h1 class="title article--full__title vspace--full">
             <?php sheepie_partial('posttitle'); ?>
-        </h2>
+        </h1>
+        <?php if (!is_page()) : ?>
+            <?php echo sheepie_postmeta('h3', 'text--small noprint vspace--half'); ?>
+        <?php endif; ?>
     </header>
     <div class="article__content">
         <?php the_content(__('Read the rest of this post &raquo;', 'sheepie')); ?>
@@ -37,21 +34,16 @@ $post_classes = array('article', 'article--full', 'vspace--half');
 
     <?php if (is_single()) : ?>
         <?php sheepie_partial('pagination', 'post'); ?>
-        <hr class="vcenter--double">
-        <footer class="article--full__footer avatar">
-            <div class="article--full__avatar avatar__container noprint">
-                <?php sheepie_avatar_background_html($author, 96, 'avatar__photo'); ?>
-            </div>
-            <div class="article--full__author avatar__text">
-                <h4 class="vspace--half">by <?php the_author_meta('display_name'); ?></h4>
-                <h4 class="article--full__meta font--small vspace--quarter"><?php sheepie_postmeta(); ?></h4>
-                <p class="article--full__bio"><?php the_author_meta('user_description'); ?></p>
-                <p class="article--full__tags">
-                    <span class="font--small"><?php the_tags(__('Tagged: ', 'sheepie'), ', '); ?></span>
-                </p>
+        <footer class="article--full__footer">
+            <hr class="vcenter--double">
+            <div class="article--full__author">
+                <?php sheepie_avatar_background_html(get_the_author_meta('ID'), 150, 'article--full__avatar'); ?>
+                <div class="article--full__bio">
+                    <h3 class="article--full__bio-blurb vspace--quarter">by <?php the_author_meta('display_name'); ?></h3>
+                    <h4 class="text--small vspace--quarter"><?php the_tags(__('Tagged: ', 'sheepie'), ', '); ?></h4>
+                    <p class="article--full__bio-blurb text--italic"><?php the_author_meta('user_description'); ?></p>
+                </div>
             </div>
         </footer>
-    <?php else: ?>
-        <hr class="vcenter--triple">
     <?php endif; ?>
 </article>
