98d4a73b3f6687b4437768482c1abe21910fc404
Mark Grealish <mark@bhalash.com>
2016-04-17 20:25:25 +0000

    Added text/meta styling classes to searchform.

--- searchform.php
+++ searchform.php
@@ -26,14 +26,14 @@ $result = __('result', 'sheepie') . (abs($total) !== 1 ? 's' : '');
         <input class="searchform__input" id="searchform__input" name="s" placeholder="<?php _e('search', 'sheepie'); ?>" type="text" required="required" value="<?php printf($query); ?>">
     </fieldset>
 </form>
-<div class="clearfix noprint searchform__meta">
+<div class="clearfix noprint searchform__meta meta">
     <span class="searchform__meta--left left-float"><?php printf('%d %s', $total, $result); ?></span>
     <?php if (function_exists('arc_search_url')) : ?>
-        <span class="searchform__meta--right right-float">
+        <span class="right-float">
             Sort by:
 
-            <a class="searchform__sort--by-oldest" href="<?php arc_search_url('asc'); ?>"><?php _e('oldest', 'sheepie'); ?></a> |
-            <a class="searchform__sort--by-newest" href="<?php arc_search_url('desc'); ?>"><?php _e('newest', 'sheepie'); ?></a>
+            <a href="<?php arc_search_url('asc'); ?>"><?php _e('oldest', 'sheepie'); ?></a> |
+            <a href="<?php arc_search_url('desc'); ?>"><?php _e('newest', 'sheepie'); ?></a>
         </span>
     <?php endif; ?>
 </div>
