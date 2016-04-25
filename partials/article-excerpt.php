3a57ea497953976df6e8981ff9b458f383841a78
Mark Grealish <mark@bhalash.com>
2016-04-25 20:38:32 +0000

    Removed reference to removed partial.

--- partials/article-excerpt.php
+++ partials/article-excerpt.php
@@ -17,7 +17,7 @@
 <article <?php post_class(['article', 'excerpt', 'vspace--full']); ?> id="article--<?php the_ID(); ?>">
     <header class="excerpt__header">
         <h3 class="excerpt__title title">
-            <?php sheepie_partial('posttitle'); ?>
+            <?php printf('<a class="%s" href="%s">%s</a>', 'navbar__title-link', get_the_permalink(), get_the_title()); ?>
         </h3>
         <?php if (!is_page()) : ?>
             <span class="postmeta text--small"><?php sheepie_postmeta(); ?></span>
