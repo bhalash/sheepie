d20aea98137b8c3ea052052b22825ca16b788cd7
Mark Grealish <mark@bhalash.com>
2016-04-26 12:53:30 +0000

    CSS class tweaks.

--- partials/gohome.php
+++ partials/gohome.php
@@ -14,6 +14,6 @@
 
 ?>
 
-<nav class="noprint navigation--home vspace--double">
-    <span class="meta"><a class="navigation--home__link" href="<?php printf(site_url()); ?>"><?php _e('Back to Home', 'sheepie'); ?></a></span>
+<nav class="noprint navigation--home">
+    <span class="meta"><a class="navigation--home__link" rel="home" href="<?php printf(site_url()); ?>"><?php _e('Back to Home', 'sheepie'); ?></a></span>
 </nav>
