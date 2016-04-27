48e31c7b75e8cdc73862d3f712ff723be165bcd8
Mark Grealish <mark@bhalash.com>
2016-04-27 21:07:02 +0000

    Rewrote KnockoutJS bindings as jQuery pub/sub.

--- assets/js/sheepie.js
+++ assets/js/sheepie.js
@@ -10,190 +10,78 @@
  * @link       https://github.com/bhalash/sheepie
  */
 
-(function() {
-    /*
-     * Wrap Pre Elements
+(function($, document, window) {
+    /**
+     * Event Subscribe/Unsubscribe
      * -------------------------------------------------------------------------
-     * highlight.js operates on the <code> child or <pre> elements. This wraps
-     * the content of an element with <code> before initializing highlight.js.
-     *
-     * This does the same thing as jQuery's $(foo).wrapInner(), albeit with less
-     * regex selector magic.
-     *
-     *  if ($('pre').length) {
-     *      $('pre:not(:has(> code))').wrapInner('<code></code>');
-     *  }
      */
 
-    function wrapInsideElement(selector, wrapper) {
-        selector = document.querySelectorAll(selector);
-        wrapper = wrapper.replace(/(^<.*\/|>$)/g, '');
+    var observer = $({});
 
-        [].forEach.call(selector, function(element) {
-            if (element.querySelectorAll(wrapper).length === 0) {
-                var newChild = document.createElement(wrapper);
+    var actions = {
+        trigger: 'publish',
+        on: 'subscribe',
+        off: 'unsubscribe'
+    };
 
-                while (element.childNodes.length) {
-                    newChild.appendChild(element.childNodes[0]);
-                }
-
-                element.appendChild(newChild);
-            }
-        });
-    }
-
-    wrapInsideElement('pre', '<code></code>');
+    $.each(actions, function(name, action) {
+        $[action] = function() {
+            observer[name].apply(observer, arguments);
+        }
+    });
 
     /**
-     * Remove Photobox Breaks
-     * -------------------------------------------------------------------------
-     * WordPress can insert <br> tags between elements if it detects either a
-     * space or a line break. This can break the formatting. Runs once.
-     *
-     * Does exactly the same thing as jQuery's $(foo).remove().
-     */
-
-    function removeSelector(selector) {
-        selector = document.querySelectorAll(selector);
-
-        if (selector.length) {
-            [].forEach.call(selector, function(element) {
-                element.parentNode.removeChild(element);
-            });
-        }
-    }
-
-    // WordPress sometimes inserts <br> between <a> elements inside a <figure>.
-    removeSelector('figure br');
-    // WordPress always inserts an empty <p> element before <figure> elements.
-    removeSelector('p:empty');
-
-    /*
-     * Sheepie Actions Controller
+     * Keybind Switchboard
      * -------------------------------------------------------------------------
      */
 
-    var sheepieController = function() {
-        return function() {
-            var self = this;
-
-            /*
-             * View States
-             * -----------------------------------------------------------------
-             *  Add whatever extra states here.
-             */
-
-            self.elements = {
-                search: ko.observable(false),
-                lightbox: ko.observable(false)
-            };
-
-            // Current state.
-            self.shown = null;
-
-            // Dynamic state toggle.
-            self.toggle = {};
-
-            for (var key in self.elements) {
-                self.toggle[key] = new Function('self.show("' + key + '")');
-            }
-
-            /*
-             * KnockoutJS Keybind Actions
-             * -----------------------------------------------------------------
-             *  @param  object      data        Data passed.
-             *  @param  object      event       Event and element information.
-             */
-
-            self.switchboard = function(data, event) {
-                switch(event.keyCode) {
-                    case 27:
-                        // Close whatever is open on <escape>.
-                        self.show(null);
-                        break;
-                    case 37:
-                    case 39:
-                        // Set next/previous image.
-                        self.lightbox.change(event);
-                        break;
-                }
-            };
-
-            /*
-             * Change State
-             * -----------------------------------------------------------------
-             * The modal (search, lightbox) element on this site have an
-             * exclusive appearance: only one at a time should.elements.
-             *
-             *  1. Hide all the eiements, except name.
-             *  2. If name exists, invert its state.
-             *
-             * @param   string      name        State to toggle true.
-             */
-
-            self.show = function(name) {
-                for (var i in self.elements) {
-                    if (self.shown === name) {
-                        continue;
-                    }
-
-                    self.elements[i](false);
-                }
-
-                if (name && name in self.elements) {
-                    // Invert state.
-                    self.elements[name](!self.elements[name]());
-                }
-
-                self.shown = name || null;
-            };
-
-            /*
-             * Lightbox
-             * -----------------------------------------------------------------
-             */
-
-            self.lightbox = {
-                text: ko.observable(null),
-                image: ko.observable(null),
-                link: ko.observable(null)
-            };
-
-            /*
-             * Set Lightbox Data and Open Lightbox
-             * -----------------------------------------------------------------
-             */
-
-            self.lightbox.show = function(data, event) {
-                var image = event.target;
-
-                self.lightbox.text(image.attributes.alt.value);
-                self.lightbox.image(image.attributes.src.value);
-                self.lightbox.link(image.parentNode.attributes.href.value);
-
-                self.show('lightbox');
-            };
-
-            /*
-             * Set Lightbox Data and Open Lightbox
-             * -----------------------------------------------------------------
-             */
-
-            self.lightbox.change = function(crap) {
-                if (self.shown !== 'lightbox') {
-                    return;
-                }
-
-                // TODO: On left-or-right arrow keypress, traverse the DOM
-                // for the previous/change image in the same article.
-
-                switch(crap.keyCode) {
-                    case 37: console.log('left'); break;
-                    case 39: console.log('right'); break;
-                }
-            }
+    $(document).on('keydown', function(event) {
+        switch (event.keyCode) {
+            case 27: $.publish('/modal/hide'); break;
         }
-    }
+    });
 
-    ko.applyBindings(new sheepieController());
-})();
+    /**
+     * DOM Cleanup
+     * -------------------------------------------------------------------------
+     */
+
+    $('pre').wrapInner('<code></code>');
+    $('figure br, p:empty').remove();
+
+    /**
+     * Publishers
+     * -------------------------------------------------------------------------
+     */
+
+    $(document).on('click tap', 'a img', function(event) {
+        var data = {
+            src: $(this).attr('src'),
+            alt: $(this).attr('alt')
+        };
+
+        $.publish('/article/image/click', data);
+        return false;
+    });
+
+    $('#lightbox').on('click tap', function(event) {
+        $.publish('/modal/hide');
+        return false;
+    });
+
+    /**
+     * Subscribers
+     * -------------------------------------------------------------------------
+     */
+
+    $.subscribe('/article/image/click', function(event, data) {
+        $('.lightbox__image').attr(data).ready(function() {
+            $('.lightbox').removeClass('hidden').show();
+        });
+    });
+
+    $.subscribe('/modal/hide', function(event, selector) {
+        selector = selector || '.modal';
+        $('.modal').hide();
+    });
+})(jQuery, document, window);
