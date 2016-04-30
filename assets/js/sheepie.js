0f6b626a69b547690428da35121bc2f7cdeed59c
Mark Grealish <mark@bhalash.com>
2016-04-30 22:30:12 +0000

    Added handling for modal search.

--- assets/js/sheepie.js
+++ assets/js/sheepie.js
@@ -12,75 +12,178 @@
 
 (function($, document, window) {
     /**
-     * Event Subscribe/Unsubscribe
-     * -------------------------------------------------------------------------
-     */
-
-    var observer = $({});
-
-    var actions = {
-        trigger: 'publish',
-        on: 'subscribe',
-        off: 'unsubscribe'
-    };
-
-    $.each(actions, function(name, action) {
-        $[action] = function() {
-            observer[name].apply(observer, arguments);
-        }
-    });
-
-    /**
-     * Keybind Switchboard
-     * -------------------------------------------------------------------------
-     */
-
-    $(document).on('keydown', function(event) {
-        switch (event.keyCode) {
-            case 27: $.publish('/modal/hide'); break;
-        }
-    });
-
-    /**
      * DOM Cleanup
-     * -------------------------------------------------------------------------
      */
 
     $('pre').wrapInner('<code></code>');
     $('figure br, p:empty').remove();
 
     /**
-     * Publishers
-     * -------------------------------------------------------------------------
+     * Sheepie jQuery object.
      */
 
-    $(document).on('click tap', 'a img', function(event) {
-        var data = {
-            src: $(this).attr('src'),
-            alt: $(this).attr('alt')
-        };
+    $.sheepie = {};
 
-        $.publish('/article/image/click', data);
-        return false;
+    $.sheepie.parse = function(selector) {
+        return (selector.match(/^[#\.]/)) ? selector : '.' + selector;
+    }
+
+    /**
+     * Event Subscribe/Unsubscribe
+     */
+
+    $.sheepie.observer = $({});
+
+    var actions = {
+        trigger: 'broadcast',
+        on: 'subscribe',
+        off: 'unsubscribe'
+    };
+
+    $.each(actions, function(name, action) {
+        $[action] = function() {
+            $.sheepie.observer[name].apply($.sheepie.observer, arguments);
+        }
     });
 
-    $('#lightbox').on('click tap', function(event) {
-        $.publish('/modal/hide');
+    /**
+     * Set up modal toggles to operate.
+     *
+     * @param {object} event DOM event.
+     */
+
+    $(document).on('keydown', function(event) {
+        switch (event.keyCode) {
+            case 27: $.broadcast('/modal/hide'); break;
+        }
+    });
+
+    /**
+     * Set up modal toggles to operate.
+     *
+     * @param {object} event DOM event.
+     * @returns {bool} false Prevent DOM event propagation.
+     */
+
+    $(document).on('click', '.toggle', function(event) {
+        if ($(this).data('toggle')) {
+            $.broadcast('/modal/toggle', $(this).data('toggle'));
+            return false;
+        }
+    });
+
+    /**
+     * Set lightbox image when article iamge clicked.
+     *
+     * @param {object} event DOM event.
+     * @returns {bool} false Prevent DOM event propagation.
+     */
+
+    $('article').on('click tap', 'a img', function(event) {
+        var data = {
+            src: $(this).data('src') || $(this).attr('src'),
+            alt: $(this).data('alt') || $(this).attr('alt')
+        };
+
+        $.broadcast('/article/image/click', data);
         return false;
     });
 
     /**
-     * Subscribers
-     * -------------------------------------------------------------------------
+     * Hide lightbox when clicked.
+     *
+     * @param {object} event DOM event.
+     */
+
+    $('.lightbox').on('click tap', function(event) {
+        $.broadcast('/modal/hide', 'lightbox');
+        return false;
+    });
+
+    /**
+     * Show article image in lightbox.
+     *
+     * @param {object} event DOM event.
+     * @param {object} data Image data with which to populate image.
      */
 
     $.subscribe('/article/image/click', function(event, data) {
-        $('.lightbox__image').attr(data).ready(function() {
-            $('.lightbox').removeClass('hidden').show();
-        });
+        if (data) {
+            $('.lightbox__image').attr(data).ready(function() {
+                $.broadcast('/modal/show', 'lightbox');
+            });
+        }
     });
 
-    $.subscribe('/modal/hide', function(event, selector) {
-        $(selector || '.modal').hide();
+    /**
+     * Hide all modals/named modal.
+     *
+     * @param {object} event DOM event.
+     * @param {string} modal Modal to hide.
+     */
+
+    $.subscribe('/modal/hide', function(event, modal) {
+        if (modal) {
+            modal = $.sheepie.parse(modal);
+        }
+
+        return $(modal || '.modal').hide();
+    });
+
+    /**
+     * Display named modal.
+     *
+     * @param {object} event DOM event.
+     * @param {string} modal Modal to show.
+     */
+
+    $.subscribe('/modal/show', function(event, modal) {
+        if (!modal) {
+            return false;
+        }
+
+        var type = '';
+
+        type = $.sheepie.parse(modal);
+        $('.modal').not(type).hide();
+        $(type).removeClass('hidden').show();
+        $.broadcast('/modal/shown', modal);
+    });
+
+    /**
+     * Toggle modal state.
+     *
+     * @param {object} event DOM event.
+     * @param {string} modal Modal to toggle.
+     */
+
+    $.subscribe('/modal/toggle', function(event, modal) {
+        if (!modal) {
+            return false;
+        }
+
+        var state = 'hide',
+            type = '';
+
+        type = $.sheepie.parse(modal);
+
+        if ($(type).css('display') === 'none') {
+            state = 'show';
+        }
+
+        $.broadcast('/modal/' + state, modal);
+    });
+
+    /**
+     * Modal search show callback.
+     *
+     * @param {object} event DOM event.
+     * @param {string} modal Modal to toggle.
+     */
+
+    $.subscribe('/modal/shown', function(event, modal) {
+        if (modal === 'modal-search') {
+            $('.modal-search__input').focus();
+        }
     });
 })(jQuery, document, window);
