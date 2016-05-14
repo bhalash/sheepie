5a2335b812db62c4b4aa782b5ef13fd0568d65ee
Mark Grealish <mark@bhalash.com>
2016-05-14 21:31:48 +0000

    Tweaked bindings and comments.

--- partials/modal-lightbox.php
+++ partials/modal-lightbox.php
@@ -2,7 +2,7 @@
 
 /**
  * Lightbox
- * -----------------------------------------------------------------------------
+
  * I separated this template because of the 404 switch. It was easier to wrap it
  * all up here.
  *
@@ -17,7 +17,7 @@
 
 ?>
 
-<div class="hidden lightbox modal" id="lightbox">
+<div class="hidden lightbox" data-modal="lightbox" data-click="modal:hide:lightbox" id="lightbox">
     <a class="lightbox__anchor" href="">
         <img class="lightbox__image vcenter--double" src="" alt="" />
     </a>
--- partials/modal-search.php
+++ partials/modal-search.php
@@ -2,7 +2,7 @@
 
 /**
  * Modal Search Input
- * -----------------------------------------------------------------------------
+ *
  * I separated this template because of the 404 switch. It was easier to wrap it
  * all up here.
  *
@@ -17,7 +17,7 @@
 
 ?>
 
-<div class="hidden modal-search modal noprint" id="modal-search">
+<div class="hidden modal-search noprint" data-modal="search" id="modal-search">
     <form class="modal-search__form" method="get" action="<?php echo esc_url(home_url('/')); ?>" autocomplete="off" novalidate>
         <fieldset>
             <input class="modal-search__input" id="modal-search__input" name="s" type="search" placeholder="<?php _e('search', 'sheepie'); ?>" required="required">
@@ -25,7 +25,7 @@
         </fieldset>
     </form>
     <div class="social close">
-        <a class="toggle" data-toggle="modal-search" href="">
+        <a class="toggle" data-click="modal:hide:search" href="">
             <span class="social__icon">Search</span>
         </a>
     </div>
