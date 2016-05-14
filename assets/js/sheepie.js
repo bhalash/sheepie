ebbce3eaa06efefe3c81aa07ed7aa75bfd1f0b2b
Mark Grealish <mark@bhalash.com>
2016-05-14 21:20:26 +0000

    Rewrote Sheepie JavaScript as simple data-binding framework.

--- assets/js/sheepie.js
+++ assets/js/sheepie.js
@@ -1,189 +1,169 @@
 /**
  * Sheepie JavaScript Functions
- * -----------------------------------------------------------------------------
- * @category   JavaScript File
- * @package    Sheepie
- * @author     Mark Grealish <mark@bhalash.com>
- * @copyright  Copyright (c) 2015 Mark Grealish
- * @license    https://www.gnu.org/copyleft/gpl.html The GNU GPL v3.0
- * @version    1.0
- * @link       https://github.com/bhalash/sheepie
+ *
+ * @author Mark Grealish <mark@bhalash.com>
+ * @copyright 2015 Mark Grealish
+ * @license GPL-3.0
+ * @version 1.0
+ * @link https://github.com/bhalash/sheepie
  */
 
 (function($, document, window) {
     /**
      * DOM Cleanup
+     *
+     * 1. Add <code> tag necessary for Highlight.js.
+     * 2. Remove WordPress-inserted elements which break layout in <figure>.
      */
 
-    $('pre').wrapInner('<code></code>');
-    $('figure br, p:empty').remove();
+    $('pre').wrapInner('<code/>');
+    $('figure br').add('p:empty').remove();
 
     /**
-     * Sheepie jQuery object.
+     * Event subscribe/unsubscribe.
+     *
+     * @link https://davidwalsh.name/pubsub-javascript
+     * @link https://gist.github.com/cowboy/661855
      */
 
-    $.sheepie = {};
+    $.observer = $({});
 
-    $.sheepie.parse = function(selector) {
-        return (selector.match(/^[#\.]/)) ? selector : '.' + selector;
+    /**
+     * Break up arguments object into useful data.
+     *
+     * For each action fragment (see below), broadcast an action with that
+     * fragment's data:
+     *
+     *  'modal:show:lightbox' =
+     *      'modal'
+     *      'modal:show'
+     *      'modal:show:lightbox'
+     *
+     * @param {object} args - Arguments object.
+     * @returns {array} actions - Mapped actions array.
+     *
+     */
+
+    $.observer.triggers = function(args) {
+        var args = Array.prototype.slice.apply(args),
+            actions = args[0].split(':'),
+            actionsStr = '';
+
+        return actions.map(function(action, index) {
+            if (index) {
+                actionsStr = actionsStr.concat(':');
+            }
+
+            actionsStr = actionsStr.concat(action);
+            return [actionsStr].concat(args.slice(1));
+        });
     }
 
     /**
-     * Event Subscribe/Unsubscribe
+     * Pub/sub actions.
+     *
+     * See the link for detailed information.
+     *
+     * @link https://gist.github.com/cowboy/661855
      */
 
-    $.sheepie.observer = $({});
-
-    var actions = {
-        trigger: 'broadcast',
-        on: 'subscribe',
-        off: 'unsubscribe'
-    };
-
-    $.each(actions, function(name, action) {
+    $.each({ on: 'subscribe', off: 'unsubscribe' }, function(name, action) {
         $[action] = function() {
-            $.sheepie.observer[name].apply($.sheepie.observer, arguments);
+            $.observer[name].apply($.observer, arguments);
         }
     });
 
+    $.broadcast = function() {
+        $.observer.triggers(arguments).forEach(function(action) {
+            $.observer.trigger.apply($.observer, action);
+        });
+    }
+
     /**
-     * Set up modal toggles to operate.
+     * Prevent click action from triggering when data-click elements are, well,
+     * clicked.
      *
-     * @param {object} event DOM event.
+     * @param {object} event - DOM event.
+     * @param {bool} false - Prevent normal click action from occurring.
+     */
+
+    $(document).on('click tap', '[data-click]', function(event) {
+        var element = event.currentTarget;
+        $.broadcast($(element).data('click'), [$(element).data(), element]);
+        return false;
+    });
+
+    /**
+     * Broadcast to hide all modals when <Esc> is depressed.
+     *
+     * @param {object} event - DOM event.
      */
 
     $(document).on('keydown', function(event) {
         switch (event.keyCode) {
-            case 27: $.broadcast('/modal/hide'); break;
+            case 27: $.broadcast('modal:hide'); break;
         }
     });
 
     /**
-     * Set up modal toggles to operate.
+     * Show lightbox when action click detected.
      *
-     * @param {object} event DOM event.
-     * @returns {bool} false Prevent DOM event propagation.
+     * @param {object} event - DOM event.
+     * @param {object} data - Binding event data.
+     * @param {object} element - Event triggering element.
      */
 
-    $(document).on('click', '.toggle', function(event) {
-        if ($(this).data('toggle')) {
-            $.broadcast('/modal/toggle', $(this).data('toggle'));
-            return false;
-        }
-    });
-
-    /**
-     * Set lightbox image when article iamge clicked.
-     *
-     * @param {object} event DOM event.
-     * @returns {bool} false Prevent DOM event propagation.
-     */
-
-    $('article').on('click tap', 'a img', function(event) {
-        var data = {
-            src: $(this).data('src') || $(this).attr('src'),
-            alt: $(this).data('alt') || $(this).attr('alt')
+    $.subscribe('modal:show:lightbox', function(event, data, element) {
+        data = {
+            src: $(element).data('src') || $(element).attr('src'),
+            alt: $(element).data('alt') || $(element).attr('alt')
         };
 
-        $.broadcast('/article/image/click', data);
-        return false;
+        $('.lightbox__image').attr(data).ready(function() {
+            $.broadcast('modal:show', { target: 'lightbox' });
+        });
     });
 
     /**
-     * Hide lightbox when clicked.
-     *
-     * @param {object} event DOM event.
+     * Focus modal search input on click.
      */
 
-    $('.lightbox').on('click tap', function(event) {
-        $.broadcast('/modal/hide', 'lightbox');
-        return false;
+    $.subscribe('modal:show:search', function() {
+        $('.modal-search__input').focus();
     });
 
     /**
-     * Show article image in lightbox.
+     * Show specified modal.
      *
-     * @param {object} event DOM event.
-     * @param {object} data Image data with which to populate image.
+     * Show modal if data is set.
+     *
+     * @param {object} event - DOM event.
+     * @param {object} data - Binding event data.
      */
 
-    $.subscribe('/article/image/click', function(event, data) {
-        if (data) {
-            $('.lightbox__image').attr(data).ready(function() {
-                $.broadcast('/modal/show', 'lightbox');
-            });
+    $.subscribe('modal:show', function(event, data) {
+        if (data && data.target) {
+            $.broadcast('modal:hide');
+            $('[data-modal="' + data.target + '"]').removeClass('hidden').show();
         }
     });
 
     /**
-     * Hide all modals/named modal.
+     * Hide specified/all modals.
      *
-     * @param {object} event DOM event.
-     * @param {string} modal Modal to hide.
+     * If there is no data set, hide all modals.
+     *
+     * @param {object} event - DOM event.
+     * @param {object} data - Binding event data.
      */
 
-    $.subscribe('/modal/hide', function(event, modal) {
-        if (modal) {
-            modal = $.sheepie.parse(modal);
+    $.subscribe('modal:hide', function(event, data) {
+        var modal = '[data-modal]';
+
+        if (data && data.target) {
+            modal = '[data-modal="' + data.target + '"]';
         }
 
-        return $(modal || '.modal').hide();
-    });
-
-    /**
-     * Display named modal.
-     *
-     * @param {object} event DOM event.
-     * @param {string} modal Modal to show.
-     */
-
-    $.subscribe('/modal/show', function(event, modal) {
-        if (!modal) {
-            return false;
-        }
-
-        var type = '';
-
-        type = $.sheepie.parse(modal);
-        $('.modal').not(type).hide();
-        $(type).removeClass('hidden').show();
-        $.broadcast('/modal/shown', modal);
-    });
-
-    /**
-     * Toggle modal state.
-     *
-     * @param {object} event DOM event.
-     * @param {string} modal Modal to toggle.
-     */
-
-    $.subscribe('/modal/toggle', function(event, modal) {
-        if (!modal) {
-            return false;
-        }
-
-        var state = 'hide',
-            type = '';
-
-        type = $.sheepie.parse(modal);
-
-        if ($(type).css('display') === 'none') {
-            state = 'show';
-        }
-
-        $.broadcast('/modal/' + state, modal);
-    });
-
-    /**
-     * Modal search show callback.
-     *
-     * @param {object} event DOM event.
-     * @param {string} modal Modal to toggle.
-     */
-
-    $.subscribe('/modal/shown', function(event, modal) {
-        if (modal === 'modal-search') {
-            $('.modal-search__input').focus();
-        }
+        $(modal).hide();
     });
 })(jQuery, document, window);
